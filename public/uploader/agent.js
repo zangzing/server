3d50cb40173d439fded42ec0697f2f6841744e1c
jeremyhermann <jeremyhermann@gmail.com>
2010-05-21 11:09:42 -0700

    cleanup agent command urls and other path stuff

--- public/uploader/agent.js
+++ public/uploader/agent.js
@@ -1,45 +1,59 @@
 var agent =  {
-		
+
 	getFilesAsync: function(path, onSuccess, onError)
 	{
-		agent.callAgentAsync("listdir", path, null, onSuccess, onError)
+		agent.callAgentAsync("listdir", {"path":path}, onSuccess, onError)
 	},
-	
+
 
 	uploadAsync: function(path, albumId, onSuccess, onError)
 	{
-		agent.callAgentAsync("upload", path, albumId, onSuccess, onError)
-	},
-	
-	cancelUploadAsync: function(path, onSuccess, onError)
-	{
-		agent.callAgentAsync("cancel_upload", path, null, onSuccess, onError)
-	},
-	
-	
-	getUploadStatsAsync: function(onSuccess, onError)
-	{
-		agent.callAgentAsync("upload_stats",null, null, onSuccess, onError)
-	},
-	
-	getThumbnailUrl: function(path)
-	{
-		return "http://localhost:9090/thumbnail/null/" + path;
+ 		agent.callAgentAsync("upload", {"path":path, "albumid":albumId}, onSuccess, onError)
 	},
 
-	
-	callAgentAsync: function(command, param1, param2, onSuccess, onError)
+	cancelUploadAsync: function(path, onSuccess, onError)
 	{
-		$.ajax({
-            url: "http://localhost:9090/" + command + "/" + param2 + "/" + param1,
-            dataType: "json", 
+		agent.callAgentAsync("cancel_upload", {"path":path}, onSuccess, onError)
+	},
+
+
+	getUploadStatsAsync: function(onSuccess, onError)
+	{
+		agent.callAgentAsync("upload_stats",{}, onSuccess, onError)
+	},
+
+	getThumbnailUrl: function(path)
+	{
+		return "http://localhost:9090/thumbnail?path=" + path;
+	},
+
+
+	callAgentAsync: function(command, params, onSuccess, onError)
+	{
+		var query = ""
+
+        for(name in params)
+        {
+            if(query!="")
+            {
+                query+="&";
+            }
+            query+=name;
+            query+="="
+            query+=encodeURIComponent(params[name]);
+        }
+
+
+        $.ajax({
+            url: "http://localhost:9090/" + command + "?" + query,
+            dataType: "json",
             success:function(response){
 				onSuccess(response)
             },
             error:function (xhr, ajaxOptions, thrownError){
 				alert("error in '/" + command + "':" + thrownError);
 				onError(thrownError);
-            }    
-        });		
+            }
+        });
 	}
 }
