84c7076be05613bb5540edb3ad41bedcb1807d83
jeremyhermann <jeremyhermann@gmail.com>
2010-08-03 10:07:54 -0700

    removing line that sets photo status. this breaks the workflow defined in photo.rb

--- lib/general_import_request.rb
+++ lib/general_import_request.rb
@@ -3,7 +3,7 @@ class GeneralImportRequest < Struct.new(:photo_id, :source_url)
   def perform
     photo = Photo.find(photo_id)
     photo.local_image = RemoteFile.new(source_url)
-    photo.state = 'loaded' if photo.local_image?
+    #photo.state = 'loaded' if photo.local_image?
     photo.save
   end
 
