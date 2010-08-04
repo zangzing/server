f5b6da5af3d2cab617820b4e8125c5f4a899bba1
Mauricio Alvarez <mauricio@nextstagellc.com>
2010-08-03 23:37:56 -0700

    adjustin email settings

--- cruise_config.rb
+++ cruise_config.rb
@@ -3,10 +3,10 @@
 Project.configure do |project|
 
   # Send email notifications about broken and fixed builds to email1@your.site, email2@your.site (default: send to nobody)
-  # project.email_notifier.emails = ["dev@zangzing.com"]
+  project.email_notifier.emails = ["dev@zangzing.com"]
 
   # Set email 'from' field to john@doe.com:
-  # project.email_notifier.from = 'server-cruisecontrol@zangzing.com'
+  # project.email_notifier.from = 'cruisecontrol@zangzing.com'
 
   # Build the project by invoking rake task 'custom'
   # project.rake_task = 'custom'
