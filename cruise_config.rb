27913f8c2aaa305e13bcc3247ed5090fd1de3cf4
Mauricio Alvarez <mauricio@nextstagellc.com>
2010-08-03 23:02:12 -0700

    added cruise config

--- cruise_config.rb
+++ cruise_config.rb
@@ -3,7 +3,7 @@
 Project.configure do |project|
 
   # Send email notifications about broken and fixed builds to email1@your.site, email2@your.site (default: send to nobody)
-  project.email_notifier.emails = ["dev@zangzing.com"]
+  # project.email_notifier.emails = ["dev@zangzing.com"]
 
   # Set email 'from' field to john@doe.com:
   # project.email_notifier.from = 'server-cruisecontrol@zangzing.com'
