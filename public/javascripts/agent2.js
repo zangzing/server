1d06b080c07006f90dda162ef34783f313bc40a5
jeremyhermann <jeremyhermann@gmail.com>
2010-08-15 22:31:39 -0700

    progress on new filechooser

---
+++ public/javascripts/agent2.js
@@ -0,0 +1,118 @@
+//
+// agent.js
+//
+// Copyright �2010, ZangZing LLC. All rights reserved.
+//
+
+
+var agent = {
+
+    port : 9090,
+    agentId: null,
+    
+    isAvailable: function(callback) {
+
+        var onSuccess = function() {
+            callback(true)
+        }
+
+        var onError = function() {
+            callback(false)
+        }
+
+
+        this.callAgent("/ping", onSuccess, onError)
+
+    },
+
+
+    getAgentId: function(onSuccess, onError){
+        if(this.agentId != null){
+            onSuccess(this.agentId);
+        }
+        else{
+            var me = this;  //so we can use in handler function
+
+
+            var successHandler = function(json){
+                me.agentId = json['agent_id']
+                onSuccess(me.agentId)
+            }
+
+            this.callAgent("/ping", successHandler, onError);
+        }
+    },
+
+    getFiles: function(virtualPath, onSuccess, onError) {
+        this.callAgent("/files/" + encodeURIComponent(virtualPath), onSuccess, onError)
+    },
+
+    getRoots: function(onSuccess, onError) {
+        this.callAgent("/roots", onSuccess, onError)
+    },
+
+
+    uploadPhoto: function(albumId, virtualPath, onSuccess, onError) {
+        this.callAgent("/albums/" + albumId + "/photos/upload?path=" + encodeURIComponent(virtualPath), onSuccess, onError)
+    },
+
+    cancelUpload : function(albumId, photoId, onSuccess, onError) {
+        this.callAgent("/albums/" + albumId + "/photos/" + photoId + "/cancel_upload", onSuccess, onError)
+    },
+
+
+    getThumbnailUrl: function(path, hint) {
+        var user_session = $.cookie("user_credentials");
+        var url = "http://localhost:" + this.port + "/files/" + encodeURIComponent(path) + "/thumbnail?session=" +user_session;
+        if (hint && hint.length > 0) {
+            url += "&hint=" + hint;
+        }
+        return url;
+    },
+
+
+    callAgent: function(path, onSuccess, onError) {
+        var url;
+        var user_session = $.cookie("user_credentials");
+        if (path.indexOf('?') == -1) {
+            url = "http://localhost:" + this.port + path + "?session="+user_session+"&callback=?"
+        }
+        else {
+            url = "http://localhost:" + this.port + path + "&session="+user_session+"&callback=?"
+        }
+
+
+        //this is called when the http call succeeds
+        var successHandler = function(response){
+            if(response.headers.status == 200){
+                onSuccess(response.body)
+            }
+            else{
+                //this is an error wrapped in JSON
+                errorHandler(response)
+            }
+
+        }
+
+        //this is called when the http call fails
+        var errorHandler = function(response){
+            if(response.headers){
+                //this error is wrapped in JSON
+                logger.debug("error calling agent: " + response.headers.status + ":" + response.headers.error_message + " url:  " + url )
+            }
+            else{
+                logger.debug("no response or invalid response from agent. url: " + url )
+            }
+            
+            if(typeof(onError) != 'undefined'){
+                onError()  
+            }
+        }
+
+        $.jsonp({
+            url: url,
+            success: successHandler,
+            error: errorHandler
+        });
+    }
+}
--- public/javascripts/filechooser.js
+++ public/javascripts/filechooser.js
@@ -2,336 +2,132 @@
 
 
 
-$(function() {
-
-    // Bind an event to window.onhashchange that, when the hash changes, gets the
-    // hash and adds the class "selected" to any matching nav link.
-    $(window).bind('hashchange', function() {
-        filechooser.showPath(document.location.hash.substring(1))
-
-    })
-
-    // Since the event is only triggered when the hash changes, we need to trigger
-    // the event now, to handle the hash the page may have loaded with.
-    $(window).trigger('hashchange');
-});
-
-
 var filechooser = {
 
-    selection : [],
-    json : [],
-    imageloader : null,
-    virtualPath: null,
-    albumId: null,
+    imageloader : null, 
+
+    ancestors : [],
+
+    roots : [
+        {"open_url": "http://localhost:9090/iphoto/folders", "type": "folder", "name": "iPhoto"},
+        {"open_url": "http://localhost:9090/filesystem/folders", "type": "folder", "name": "My Computer"},
+        {"open_url": "http://localhost:9090/filesystem/folders/L1VzZXJzL2hvcGVtZW5n", "type": "folder", "name": "hopemeng"},
+        {"open_url": "http://localhost:3000/facebook/folders.json", "type": "folder", "name": "Facebook"}
+    ],
 
 
-    showPath: function(virtualPath) {
+
+    init : function(){
+        $("#filechooser-back-button").bind('click', filechooser.open_parent_folder)
+        filechooser.open_root()
+    },
+
+    open_root: function(){
+        filechooser.on_open_folder("Home", "", filechooser.roots)
+    },
+
+
+    open_folder: function(name, url){
+
+        if(url == ""){
+            filechooser.open_root()
+        }
+        else{
+            if(url.indexOf("http://localhost:9090")===0){
+                var user_session = $.cookie("user_credentials");
+
+                url += "?session="+user_session+"&callback=?";
+
+                $.jsonp({
+                    url: url,
+                    success: function(json){ filechooser.on_open_folder(name, url, json) },
+                    error: filechooser.on_error
+                });
+            }
+            else{
+                $.ajax({
+                   dataType: "json",
+                   url: url,
+                   success: function(json){ filechooser.on_open_folder(name, url, json) },
+                   error: filechooser.on_error
+                });
+            }
+        }
+    },
+
+    on_open_folder : function(name, url, children){
+        //setup the imageloader
         if (filechooser.imageloader) {
             filechooser.imageloader.stop();
         }
 
         filechooser.imageloader = new ImageLoader(function(id, src) {
             $("#" + id).attr('src', src)
-        });
-
-        filechooser.virtualPath = virtualPath;
-        filechooser.reloadChooser();
+        });   
 
 
-    },
 
-    setAlbumId: function(albumId) {
-        filechooser.albumId = albumId
-    },
-
-    reloadChooser : function() {
-
-        var callback = function(json) {
-            filechooser.json = json
-            filechooser.repaintChooser()
+        //unpack the jsonp resonse
+        if(children.body){
+            children = children.body
         }
 
+        var html=""
+        for(var i in children){
 
-        if (filechooser.virtualPath == "") {
-            agent.getRoots(callback);
+            if(children[i].type=='folder'){
+                html += "<li onclick=\"filechooser.open_folder('" + children[i].name + "','" + children[i].open_url + "')\">";
+                html += children[i].name;
+                html += "</li>";
+            }
+            else{
+                var id = "photo_" + i 
+                html += "<li>";
+                html += "<img id='"+ id +"' src=''>"
+                html += children[i].name;
+                html += "</li>";
+
+                filechooser.imageloader.add(id, children[i].thumb_url);
+
+            }
         }
-        else {
-            agent.getFiles(filechooser.virtualPath, callback);
-        }
 
-    },
-
-    repaintChooser : function() {
-        $("#filechooser-back-button").html(filechooser.buildBackButtonHtml())
-        $("#filechooser-title").html(filechooser.buildTitleHtml())
-        $("#filechooser-files").html(filechooser.buildChooserHtml(filechooser.json))
+        $("#filechooser").html(html)
 
         filechooser.imageloader.start(5);
-    },
 
 
-    selectedPhotos : function() {
-        return filechooser.selection
-    },
 
-    isSelected : function(virtual_path) {
-        for (var i in filechooser.selection) {
-            if (filechooser.selection[i]["virtual_path"] == virtual_path) {
-                return true
-            }
+        if(filechooser.ancestors.length > 0){
+            $("#filechooser-back-button").html(filechooser.ancestors[filechooser.ancestors.length-1].name);
         }
-        return false
-    },
-
-
-//    selectFolder : function(folderVirtualPath){
-//
-//        var onGetFiles = function(json){
-//            //2. add photos to 'added photos' tray
-//
-//            var photos = []
-//            for (i in json){
-//                if(json[i]['is_dir'] == false ){
-//                    var photo = {}
-//                    photos[photos.length] = photo
-//                    photo["virtual_path"] = json[i]["virtual_path"]
-//                    photo["photo_id"] = null
-//                    filechooser.selection.push(photo)
-//                }
-//            }
-//
-//
-//            var onCreatePhotos = function(json){
-//                //4. update photo ids of items in 'added photos' tray
-//                for(i in photos){
-//                    photos[i]['photo_id'] = json[i]['id']
-//                }
-//                filechooser.repaintSelection(); //we now have the photo_id, so we can display the close box
-//
-//                //5. call the agent to upload each photo
-//                for(i in photos){
-//                    //todo: create agent api that uploads contents of folder given a virtual_path and a list of photo ids
-//                    agent.uploadPhoto(filechooser.albumId, photos[i]['photo_id'], photos[i]['virtual_path'], function(){}, function(){});
-//                }
-//
-//
-//            }
-//
-//            //3. create photo object on server for each photo in folder
-//            server.createMultiplePhotos(filechooser.albumId, photos.length, onCreatePhotos, function(){})
-//        }
-//
-//
-//        //1. get contents of folder from the agent
-//        agent.getFiles(folderVirtualPath, onGetFiles);
-//
-//    },
-
-    selectPhoto : function(virtual_path) {
-        var photo = {}
-        photo["virtual_path"] = virtual_path
-        photo["photo_id"] = null
-
-
-        filechooser.selection.push(photo)
-        filechooser.repaintSelection() //moves picture to selection tray
-//        filechooser.repaintChooser() //very inefficiently change clicked photo to gray
-
-
-        var onCreatePhoto = function(json){
-            var photoId = json["id"];
-
-            photo["photo_id"] = photoId;
-            filechooser.repaintSelection(); //we now have the photo_id, so we can display the close box
-
+        else{
+            $("#filechooser-back-button").html("");
         }
 
-        agent.uploadPhoto(filechooser.albumId, virtual_path, onCreatePhoto, function(){});
+        $("#filechooser-title").html(name);
 
-
-
-    },
-
-    unselectPhoto : function(photoId) {
-        var list = [];
-        for (var i in filechooser.selection) {
-            var test = filechooser.selection[i]
-            if (test["photo_id"] != photoId) {
-                list[list.length] = test
-            }
-        }
-        filechooser.selection = list
-        filechooser.repaintSelection()
-//        filechooser.reloadChooser()
-        
-
-        server.destroyPhoto(filechooser.albumId, photoId, function(){}, function(){})
-        agent.cancelUpload(filechooser.albumId, photoId, function(){}, function(){})
-
-    },
-
-    repaintSelection : function() {
-        $("#filechooser-selection").html(filechooser.buildSelectionHtml())
-        $("#filechooser-selection").scrollLeft(10000)
-    },
-
-
-    buildSelectionHtml : function() {
-        var html = ""
-        // var width = 200 * filechooser.selection.length
-
-        for (var i in filechooser.selection) {
-
-            var photo = filechooser.selection[i]
-
-
-            var hint = ""
-            if (photo.hint_thumb_path) {
-                hint = "hint=" + encodeURIComponent(file.hint_thumb_path)
-            }
-
-
-            html += "<div style='float:left'>"
-            html += "<img height='75' style='margin-left:8px; border:4px solid white' src='" + agent.getThumbnailUrl(photo['virtual_path'], hint) + "'>";
-
-            if (photo["photo_id"] != null) {
-                html += "<a style=\"float:right;margin-left:0px\" href=\"\" onclick=\"filechooser.unselectPhoto('" + photo['photo_id'] + "');return false;\">"
-                html += "(x)"
-                html += "</a>"
-            }
-            html += "</div>"
-
-        }
-        return html
-
-    },
-
-    buildChooserHtml: function(json) {
-
-        var html = ""
-
-        for (var i = 0; i < json.length; i++) {
-
-            html += "<div style='text-align:center; width:180px; height:140px; float:left; border:0px'>"
-
-
-            var file = json[i]
-
-            if (file.is_dir == true) {
-                //html += "<a href=\"\" onclick=\"filechooser.selectFolder(\'" + file.virtual_path + "\');return false;\">"
-                html += "<img height='100' src='/images/folder.jpeg'>"
-                //html += "</a>"
-            }
-            else {
-                var BOX = 125
-                var width;
-                var height;
-
-                if (file.aspect_ratio) {
-                    if (file.aspect_ratio == 1) {
-                        height = BOX
-                        width = BOX
-                    }
-                    else if (file.aspect_ratio < 1) {
-                        height = BOX
-                        width = BOX * file.aspect_ratio
-                    }
-                    else {
-                        width = BOX
-                        height = BOX / file.aspect_ratio
-                    }
-                }
-                else {
-                    height = BOX
-                    width = BOX
-                }
-
-
-                var hint = ""
-                if (file.hint_thumb_path) {
-                    hint = encodeURIComponent(file.hint_thumb_path)
-                }
-
-
-                var url = agent.getThumbnailUrl(file.virtual_path, hint)
-                var id = "thumbnail_" + i
-
-                filechooser.imageloader.add(id, url);
-
-                if (!filechooser.isSelected(file.virtual_path)) {
-                    html += "<a href=\"\" onclick=\"filechooser.selectPhoto(\'" + file.virtual_path + "\');return false;\">"
-                    html += "<img id='" + id + "' height='" + height + "' width='" + width + "' style='border:4px solid white' src='/images/blank.gif'>"
-                    html += "</a>"
-                }
-                else {
-                    html += "<img id='" + id + "' height='" + height + "' width='" + width + "' style='border:4px solid #AAAAAA' src='/images/blank.gif'>"
-                }
-
-
-            }
-
-            html += "<br/>"
-
-            if (file.is_dir == true) {
-                html += "<a href='#" + file.virtual_path + "'>"
-                html += file.name
-                html += "</a>"
-            }
-
-            html += "</div>"
-        }
-        return html
+        filechooser.ancestors.push({name:name, url:url});
 
     },
 
 
-    buildBreadCrumbs : function() {
-        var breadcrumbs = []
-        var path = filechooser.virtualPath
-
-
-        for (; ;) {
-            var breadcrumb = {}
-            var i = path.lastIndexOf('/')
-
-            if (i == -1) {
-                breadcrumb["title"] = "Home"
-                breadcrumb["virtual_path"] = ""
-                breadcrumbs[breadcrumbs.length] = breadcrumb
-                break
-            }
-            else {
-                breadcrumb["title"] = path.substring(i + 1)
-                breadcrumb["virtual_path"] = path
-                breadcrumbs[breadcrumbs.length] = breadcrumb
-
-                path = path.substring(0, i)
-            }
-
-        }
-        return breadcrumbs.reverse()
+    open_parent_folder: function()
+    {
+        var current = filechooser.ancestors.pop(); //discard this
+        var parent = filechooser.ancestors.pop();
+        filechooser.open_folder(parent.name, parent.url);
     },
 
-    buildBackButtonHtml: function() {
-        var breadcrumbs = filechooser.buildBreadCrumbs()
-        breadcrumbs.pop()
-        var crumb = breadcrumbs.pop()
-        if (crumb) {
-            html = "<a href='#" + crumb["virtual_path"] + "'>"
-            html += "&lt;"
-            html += crumb["title"]
-            html += "]"
-            html += "</a>"
-            return html
-        }
-        else {
-            return ""
-        }
 
-    },
-
-    buildTitleHtml: function() {
-        return "[" + filechooser.buildBreadCrumbs().pop()["title"] + "]"
+    on_error : function(error){
+        console.log("ERROR");
+        console.log(error);
+        $("#filechooser").html(error);
     }
+
 }
+
+$(document).ready(function() {
+    filechooser.init()
+});
