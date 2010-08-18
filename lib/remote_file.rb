1e259f114369d4f6435ae2dc3337c73de1da1402
duhast <shtepsel_duhast@mail.ru>
2010-08-19 00:27:05 +0300

    [ZZG-35] Making chunked download in RemoteFile

--- lib/remote_file.rb
+++ lib/remote_file.rb
@@ -1,8 +1,9 @@
-require 'open-uri'
+require 'net/http'
 require 'digest/sha1'
 
 class RemoteFile < ::Tempfile
   CONTENT_DISPOSITION_FILENAME_REGEX = /filename=([A-Z,a-z,0-9_#-]+\.*[A-Z,a-z,0-9]*)/
+  CHUNK_SIZE = 4096
 
   def initialize(path, tmpdir = Dir::tmpdir, options = {})
     @original_filename  = File.basename(path)
@@ -19,18 +20,23 @@ class RemoteFile < ::Tempfile
   end
 
   def fetch
-    string_io = OpenURI.send(:open, @remote_path, @options)
-    @content_type = string_io.meta['content-type']
-    if string_io.meta['content-disposition'] =~ CONTENT_DISPOSITION_FILENAME_REGEX
-      @original_filename = $1
+    uri = URI::parse(@remote_path)
+    self.binmode if is_windows?
+    Net::HTTP.start(uri.host, uri.port) do |http|
+      http.request_get(uri.path, @options) do |remote_side|
+        @content_type = remote_side.header['content-type']
+        if remote_side.header['content-disposition'] =~ CONTENT_DISPOSITION_FILENAME_REGEX
+          @original_filename = $1
+        end
+        remote_side.read_body do |chunk|
+          self.write chunk
+        end
+      end
     end
     unless @original_filename.include?('.') && (@content_type == 'x-octet-stream')
       extension = @content_type.split('/').last
       @original_filename += ".#{extension}"
     end
-
-    self.binmode if is_windows?
-    self.write string_io.read
     self.rewind
     self
   end
