69b959b90ed4349517498861b0a3222690f004b5
jeremyhermann <jeremyhermann@gmail.com>
2010-08-20 13:50:50 -0700

    fix copy/paste error

--- app/controllers/flickr_folders_controller.rb
+++ app/controllers/flickr_folders_controller.rb
@@ -26,12 +26,12 @@ class FlickrFoldersController < FlickrController
       #todo: refactor this so that flickr_folders_controller and flickr_photos_controller can share
       photo_url = get_photo_url(p, :full)
       photo = Photo.create(
-                :caption => info.title,
+                :caption => p.title,
                 :album_id => params[:album_id],
                 :user_id=>current_user.id,
                 :source_guid => Photo.generate_source_guid(photo_url),
-                :source_thumb_url => get_photo_url(info, :thumb),
-                :source_screen_url => get_photo_url(info, :screen)
+                :source_thumb_url => get_photo_url(p, :thumb),
+                :source_screen_url => get_photo_url(p, :screen)
       )
 
       Delayed::Job.enqueue(GeneralImportRequest.new(photo.id, photo_url))
