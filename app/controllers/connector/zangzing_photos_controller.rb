ea16b82ce0874f01eca3b7f1c42c13881a2f000f
duhast <shtepsel_duhast@mail.ru>
2010-09-09 22:29:55 +0300

    [ZZG-48] ZZ connector added

---
+++ app/controllers/connector/zangzing_photos_controller.rb
@@ -0,0 +1,42 @@
+class Connector::ZangzingPhotosController < Connector::ConnectorController
+
+  def index
+    @photos = current_user.albums.find(params[:zz_album_id]).photos.map do |p|
+      {
+        :name => p.caption,
+        :id   => p.id,
+        :type => 'photo',
+        :thumb_url => p.thumb_url,
+        :screen_url => p.medium_url,
+        :add_url => zangzing_photo_action_path({:photo_id => p.id, :action => 'import'}),
+        :source_guid => p.source_guid
+      }
+    end
+
+    render :json => @photos.to_json
+  end
+
+#  def show
+#    size_wanted = (params[:size] || 'screen').downcase.to_sym
+#    photo_url = get_photo_url(params[:photo_id], PHOTO_SIZES[size_wanted])
+#    bin_io = OpenURI.send(:open, photo_url)
+#    send_data bin_io.read, :type => bin_io.meta['content-type'], :disposition => 'inline'
+#  end
+
+  def import
+    source_photo = current_user.albums.find(params[:zz_album_id]).photos.find(params[:photo_id])
+    photo = Photo.create(
+              :caption => source_photo.caption,
+              :album_id => params[:album_id],
+              :user_id => source_photo.user_id,
+              :source_guid => source_photo.source_guid,
+              :source_thumb_url => source_photo.source_thumb_url,
+              :source_screen_url => source_photo.source_screen_url
+    )
+
+    #Delayed::IoBoundJob.enqueue(ZzCopyRequest.new(photo.id, source_photo.id))
+    Delayed::IoBoundJob.enqueue(GeneralImportRequest.new(photo.id, source_photo.image.url))
+    render :json => photo.to_json
+  end
+
+end
---
+++ app/controllers/sendgrid_controller.rb
@@ -0,0 +1,42 @@
+class SendgridController < ApplicationController
+
+  def import
+=begin
+each album has an email address in the form <album_id>@sendgrid-post.zangzing.com
+    *      text - Text body of email. If not set, email did not have a text body.
+    *      html - HTML body of email. If not set, email did not have an HTML body.
+    *      to - Email recipient.
+    *      from - Email sender.
+    *      subject - Email Subject.
+    *      dkim - A JSON string containing the verification results of any dkim and domain keys signatures in the message.
+    *      spam_score - Spam Assassin's rating for whether or not this is spam.
+    *      spam_report - Spam Assassin's spam report.
+    *      attachments - Number of attachments included in email.
+    *      attachment1, attachment2, …, attachmentN - File upload names. The numbers are sequence numbers starting from 1 and ending on the number specified by the attachments parameter. If attachments is 0, there will be no attachment files. If attachments is 3, parameters attachment1, attachment2, and attachment3 will have file uploads.
+=end
+    album_id = params[:to].split('@').first
+    album = Album.find_by_id(album_id)
+    sender_mail = params[:from]
+    if request.post? && album && sender_mail==album.user.email
+      photos_count = params[:attachments].to_i
+      1.upto(photos_count) do |attach_index|
+        attached_image = params["attachment#{attach_index}"]
+        photo = Photo.new(
+                :caption => attached_image.original_filename,
+                :album_id => album_id,
+                :user_id => album.user.id,
+                :source_guid => Photo.generate_source_guid("#{params[:html]}_#{params[:text]}_#{Time.now.to_i}_#{attach_index}")
+        )
+        photo.local_image = attached_image
+        photo.save
+        #photo.source_thumb_url = photo.local_image.url(:thumb)
+        #photo.source_screen_url = photo.local_image.url(:medium)
+        #photo.save
+        photo.queue_upload_to_s3
+        album.photos << photo
+      end
+    end
+    render :nothing => true, :status => :ok
+  end
+
+end
