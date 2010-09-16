e8e74aa93451bfaf6919b71b4d29735598a3537c
jeremyhermann <jeremyhermann@gmail.com>
2010-09-16 11:09:07 -0700

    wiring up view buttons on album

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,9 +1,10 @@
 class PeopleController < ApplicationController
   def album_index
-    
+    @album = Album.find(params[:album_id])
   end
 
   def user_index
+    @user = User.find(params[:user_id])
   end
 
 end
