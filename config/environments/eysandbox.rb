251a8016f86224b27bb4ca953a8def7e9074d526
jeremyhermann <jeremyhermann@gmail.com>
2010-10-06 16:01:29 -0700

    clean up open graph tags on shared album

---
+++ config/environments/eysandbox.rb
@@ -0,0 +1,23 @@
+# Settings specified here will take precedence over those in config/environment.rb
+
+# In the development environment your application's code is reloaded on
+# every request.  This slows down response time but is perfect for development
+# since you don't have to restart the webserver when you make code changes.
+config.cache_classes = false
+
+# Log error messages when you accidentally call methods on nil.
+config.whiny_nils = true
+
+# Show full error reports and disable caching
+config.action_controller.consider_all_requests_local = true
+config.action_view.debug_rjs                         = true
+config.action_controller.perform_caching             = false
+
+#paperclip will look for imagemagick here
+#Paperclip.options[:command_path] = ENV['IMAGEMAGICK_PATH']
+#Paperclip.options[:log] = true
+#Paperclip.options[:log_command] = true
+
+#todo: is this the right way to handle this?
+APPLICATION_HOST = 'share2002photos.zangzing.com'
+config.action_mailer.default_url_options = {:host => APPLICATION_HOST }
