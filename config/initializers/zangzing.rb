a5bc8136df3323106288f2f1545577e8ef1216a2
Mauricio Alvarez <mauricio@nextstagellc.com>
2010-10-10 23:53:59 -0700

    Fixed APPLICATION_HOST problem

--- config/initializers/zangzing.rb
+++ config/initializers/zangzing.rb
@@ -3,13 +3,14 @@ msg = []
 msg << "=> ZangZing Initializer"
 if File.exists?( fname )
   dna =  ActiveSupport::JSON.decode( File.read( fname ))
+  APPLICATION_HOST = dna['engineyard']['environment']['apps'][0]['vhosts'][0]['domain_name']+':80'
   msg << "      Deployment information from : "+fname
   msg << "      ZangZing Server deployed at : EngineYard"
   msg << "      EngineYard environment      : "+dna['engineyard']['environment']['name']
   msg << "      Host public AWS name        : " + dna['engineyard']['environment']['instances'][0]['public_hostname']
   msg << "      Rails environment           : " + dna['engineyard']['environment']['framework_env']
-  msg << "      Domain Name                 : " + dna['engineyard']['environment']['apps'][0]['vhosts'][0]['domain_name']
-  msg << "      Port                        : 80"
+  msg << "      Domain Name                 : " + APPLICATION_HOST.split(':')[0]
+  msg << "      Port                        : " + APPLICATION_HOST.split(':')[1]
   msg << "      Source Repo                 : " + dna['engineyard']['environment']['apps'][0]['repository_name']
   msg << "      Source Repo Branch          : " + dna['engineyard']['environment']['apps'][0]['branch']
 else
