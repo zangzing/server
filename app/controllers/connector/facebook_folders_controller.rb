671060484be890b39120855d849739fa5d020984
duhast <shtepsel_duhast@mail.ru>
2010-10-15 15:32:44 +0300

    #178 Almums/photos sort by update time

--- app/controllers/connector/facebook_folders_controller.rb
+++ app/controllers/connector/facebook_folders_controller.rb
@@ -21,23 +21,29 @@ class Connector::FacebookFoldersController < Connector::FacebookController
     else
       album_list = facebook_graph.get(target)
       album_list.reject! { |a| a[:type] == 'profile' } #Remove 'Profile Pictures'
-      @folders = album_list.map do |f|
-        {
-          :name => f[:name] || "Created #{f[:created_time].strftime('%d %b %Y')}",
-          :type => "folder",
-          :id  =>  f[:id]
-        }
-      end
-      @folders.each do |f|
-        f[:add_url] = nil
-        if target=='me/friends'
-          f[:open_url] = facebook_folders_path(:target => "#{f[:id]}/albums")
-        elsif target.match(/\w+\/albums/)
-          f[:open_url] = facebook_photos_path(f[:id])
-          f[:add_url] = facebook_folder_action_path({:fb_album_id =>f[:id], :action => 'import'})
+      unless album_list.empty?
+        if album_list.first[:updated_time]
+          album_list.sort!{|a, b| b[:updated_time] <=> a[:updated_time] }
         end
+        @folders = album_list.map do |f|
+          {
+            :name => f[:name] || "Created #{f[:created_time].strftime('%d %b %Y')}",
+            :type => "folder",
+            :id  =>  f[:id]
+          }
+        end
+        @folders.each do |f|
+          f[:add_url] = nil
+          if target=='me/friends'
+            f[:open_url] = facebook_folders_path(:target => "#{f[:id]}/albums")
+          elsif target.match(/\w+\/albums/)
+            f[:open_url] = facebook_photos_path(f[:id])
+            f[:add_url] = facebook_folder_action_path({:fb_album_id =>f[:id], :action => 'import'})
+          end
+        end
+      else
+        @folders = []
       end
-
     end
 
     render :json => @folders.to_json
