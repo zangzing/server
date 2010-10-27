4392c52a663accdb38cb9cec0045dc0b6d3c9a86
jeremyhermann <jeremyhermann@gmail.com>
2010-10-27 07:03:18 -0700

    removing unused js files

--- public/javascripts/agent.js
+++ public/javascripts/agent.js
@@ -48,7 +48,6 @@ var agent = {
             }
 
             url += 'session=' + $.cookie('user_credentials') + '&user_id=' + user_id + '&callback=?';
-//            url += 'session=' + $.cookie('user_credentials') + '&callback=?';
         }
 
         logger.debug(url);
@@ -57,49 +56,6 @@ var agent = {
     },
 
 
-//    getAgentId: function(onSuccess, onError){
-//        if(this.agentId != null){
-//            onSuccess(this.agentId);
-//        }
-//        else{
-//            var me = this;  //so we can use in handler function
-//
-//
-//            var successHandler = function(json){
-//                me.agentId = json['agent_id']
-//                onSuccess(me.agentId)
-//            }
-//
-//            this.callAgent("/ping", successHandler, onError);
-//        }
-//    },
-//
-//    getFiles: function(virtualPath, onSuccess, onError) {
-//        this.callAgent("/files/" + encodeURIComponent(virtualPath), onSuccess, onError)
-//    },
-//
-//    getRoots: function(onSuccess, onError) {
-//        this.callAgent("/roots", onSuccess, onError)
-//    },
-//
-//
-//    uploadPhoto: function(albumId, virtualPath, onSuccess, onError) {
-//        this.callAgent("/albums/" + albumId + "/photos/upload?path=" + encodeURIComponent(virtualPath), onSuccess, onError)
-//    },
-//
-//    cancelUpload : function(albumId, photoId, onSuccess, onError) {
-//        this.callAgent("/albums/" + albumId + "/photos/" + photoId + "/cancel_upload", onSuccess, onError)
-//    },
-//
-//
-//    getThumbnailUrl: function(path, hint) {
-//        var user_session = $.cookie("user_credentials");
-//        var url = "http://localhost:" + this.port + "/files/" + encodeURIComponent(path) + "/thumbnail?session=" +user_session;
-//        if (hint && hint.length > 0) {
-//            url += "&hint=" + hint;
-//        }
-//        return url;
-//    },
 
 
     callAgent: function(path, onSuccess, onError) {
