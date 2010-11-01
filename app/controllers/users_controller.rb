7a486c884230f60acb5f7a779bd6254d45ac3b88
Mauricio Alvarez <mauricio@nextstagellc.com>
2010-11-01 11:15:41 -0700

    After joining, do not log user in. Wait for activation. Redirect user to root with a message saying to go look for activation email. This commit fixes #191

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
       if @user.save_without_session_maintenance
          @user.deliver_activation_instructions!
          flash[:notice] = "Your account has been created. Please check your e-mail for your account activation instructions!"
-         redirect_back_or_default @user
+         redirect_to root_url
       else
          render :action => :new
       end
