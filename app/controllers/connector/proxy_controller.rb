ae54b4c067c6fcbf2fb3fa90ffb06be027047c93
duhast <shtepsel_duhast@mail.ru>
2010-11-16 01:03:50 +0200

    x-accel

--- app/controllers/connector/proxy_controller.rb
+++ app/controllers/connector/proxy_controller.rb
@@ -8,9 +8,9 @@ class Connector::ProxyController < ApplicationController
     url = params[:url]
     if %w(production eysandbox sandbox).include?(RAILS_ENV)
       uri = URI.parse(url)
-      #response.headers['X-Accel-Redirect'] = "/nginx_redirect/#{uri.host}#{uri.path}"
-      #render :nothing => true
-      x_accel_redirect "/nginx_redirect/#{uri.host}#{uri.path}", :disposition => 'inline'
+      response.headers['X-Accel-Redirect'] = "/nginx_redirect/#{uri.host}#{uri.path}"
+      render :nothing => true
+      #x_accel_redirect "/nginx_redirect/#{uri.host}#{uri.path}", :disposition => 'inline'
     else
       bin_io = OpenURI.send(:open, url)
       send_data bin_io.read, :type => bin_io.meta['content-type'], :disposition => 'inline'
