7137442e49af805ecd7505e417cd38eab22168c2
gregseitz <gseitz@gmail.com>
2010-12-16 20:20:35 -0800

    Made Nginx work properly for ProxyController.proxy - we now pass all calls through the Nginx rather than handling them intenrally in Rails.  Also fixed a couple of bugs in the nginx_redirect location within the nginx config files.  Added much longer timeout (15 mins) for debugging sessions.

--- app/controllers/connector/proxy_controller.rb
+++ app/controllers/connector/proxy_controller.rb
@@ -4,17 +4,12 @@ class Connector::ProxyController < ApplicationController
   #todo: need to restrict what we allow to be proxied. should probably just be smugmug
   #todo: should use nginx redirect header for production
   def proxy
-    #todo: should stream this
+    #now that we rely on nginx, all requests are proxied by nginx so we
+    #don't have to serve the request up slowly from Rails
     url = params[:url]
-    if %w(production eysandbox sandbox).include?(Rails.root)
-      uri = URI.parse(url)
-      response.headers['X-Accel-Redirect'] = "/nginx_redirect/#{uri.host}#{uri.path}"
-      render :nothing => true
-      #x_accel_redirect "/nginx_redirect/#{uri.host}#{uri.path}", :disposition => 'inline'
-    else
-      bin_io = OpenURI.send(:open, url)
-      send_data bin_io.read, :type => bin_io.meta['content-type'], :disposition => 'inline'
-    end
+    uri = URI.parse(url)
+    response.headers['X-Accel-Redirect'] = "/nginx_redirect/#{uri.host}#{uri.path}"
+    render :nothing => true
   end
 end
 
