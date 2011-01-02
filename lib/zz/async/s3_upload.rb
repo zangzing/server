5e13e880356ff791061da64f9013a007178dec06
gregseitz <gseitz@gmail.com>
2011-01-01 16:04:46 -0800

    More debugging

--- lib/zz/async/s3_upload.rb
+++ lib/zz/async/s3_upload.rb
@@ -8,9 +8,21 @@ module ZZ
           super( photo_id )
         end 
           
-        def self.perform( photo_id ) 
-          photo = Photo.find(photo_id) 
-          photo.upload_to_s3
+        def self.perform( photo_id )
+#GWS more debug code
+msg = "Processing incoming photo upload in resque task"
+puts msg
+Rails.logger.info msg
+          begin
+            photo = Photo.find(photo_id)
+            photo.upload_to_s3
+          rescue => ex
+  #GWS more debug code
+  msg = ex.backtrace.to_s
+  puts msg
+  Rails.logger.info msg
+            raise ex
+          end
         end
 
         def self.on_failure_notify_photo(e, photo_id )
