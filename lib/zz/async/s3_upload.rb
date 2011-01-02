42e59d59ae21def2c8aa2d4a0c9d494c388c57d7
gregseitz <gseitz@gmail.com>
2011-01-01 17:06:42 -0800

    Debug

--- lib/zz/async/s3_upload.rb
+++ lib/zz/async/s3_upload.rb
@@ -12,22 +12,22 @@ module ZZ
 #GWS more debug code
 msg = "Processing incoming photo upload in resque task"
 puts msg
-#Rails.logger.info msg
-#          begin
-#            photo = Photo.find(photo_id)
-#            photo.upload_to_s3
-#          rescue => ex
-#  #GWS more debug code
-#  msg = ex.backtrace.to_s
-#  puts msg
-#  Rails.logger.info msg
-#            raise ex
-#          end
+Rails.logger.info msg
+          begin
+            photo = Photo.find(photo_id)
+            photo.upload_to_s3
+          rescue => ex
+  #GWS more debug code
+  msg = ex.backtrace.to_s
+  puts msg
+  Rails.logger.info msg
+            raise ex
+          end
         end
 
         def self.on_failure_notify_photo(e, photo_id )
-#          photo = Photo.find(photo_id)
-#          photo.update_attributes(:state => 'error', :error_message => 'Failed to upload the image to S3')
+          photo = Photo.find(photo_id)
+          photo.update_attributes(:state => 'error', :error_message => 'Failed to upload the image to S3')
         end
         
     end
