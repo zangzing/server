625446b259dd5a40902d76e38a49d07784f247e6
gregseitz <gseitz@gmail.com>
2011-01-08 15:40:36 -0800

    Modify so that it keeps any IMAGEMAGICK_PATH that is already set.

--- OpenRubyMine.sh
+++ OpenRubyMine.sh
@@ -4,6 +4,6 @@ export ZANG_ZING_HOME="$PWD"
 export MAGICK_HOME="$ZANG_ZING_HOME/../agent/lib/osx/imagemagick/ImageMagick-6.6.1"
 export DYLD_LIBRARY_PATH="$MAGICK_HOME/lib:$DYLD_LIBRARY_PATH"
 export PATH="$MAGICK_HOME/bin:$PATH"
-export IMAGEMAGICK_PATH="$MAGICK_HOME"
+if [ -z "${IMAGEMAGICK_PATH}" ]; then export IMAGEMAGICK_PATH="$MAGICK_HOME"; fi
 
 open "/Applications/RubyMine 3.0.1.app"
