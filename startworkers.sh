5ace72bdf72ccfae66f00934ba5dbf970233862f
gregseitz <gseitz@gmail.com>
2011-01-07 16:51:11 -0800

    batch script to start all workers on developer machine

---
+++ startworkers.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# this script is used to start all the extra worker processes 
+# that the server needs to operate fully
+#
+# 
+if [ -z "${IMAGEMAGICK_PATH}" ]; then echo IMAGEMAGICK_PATH is not set; exit; fi
+echo 'Starting nginx'
+script/nginx &
+
+echo 'Starting redis-server'
+redis-server &
+
+echo 'Starting resque:work'
+QUEUE=* rake resque:work &
+
+# keep script blocked for last task
+echo 'Starting resque:scheduler'
+rake resque:scheduler
+
+exit;
+
+
+
+
