2df026091c2f205ab27a4f295a99e230843cb4c4
gregseitz <gseitz@gmail.com>
2011-01-12 08:19:20 -0800

    Fix bug 495 - now ensures we properly escape the url extracted from the passed argument before parsing.

--- app/controllers/connector/proxy_controller.rb
+++ app/controllers/connector/proxy_controller.rb
@@ -7,7 +7,8 @@ class Connector::ProxyController < ApplicationController
     #now that we rely on nginx, all requests are proxied by nginx so we
     #don't have to serve the request up slowly from Rails
     url = params[:url]
-    uri = URI.parse(url)
+    escaped_url = URI::escape(url.to_s)
+    uri = URI.parse(escaped_url)
     response.headers['X-Accel-Redirect'] = "/nginx_redirect/#{uri.host}#{uri.path}"
     render :nothing => true
   end
