bd5152caf5389d1d580efebb140759550ddfc368
Mauricio Alvarez <mauricio@nextstagellc.com>
2011-01-31 12:20:38 -0800

    Added contributors_added and welcome email to MailChimp Notifier

--- lib/zz/mailchimp/notifier.rb
+++ lib/zz/mailchimp/notifier.rb
@@ -9,84 +9,69 @@ module ZZ
         
         def contributors_added(contributor_id)
           contributor = Contributor.find( contributor_id )
-          @user = contributor.album.user
-          @album = contributor.album
-          @album_mail = contributor.album.short_email
+          user = contributor.album.user
+          album = contributor.album
 
-          vcard = Vpim::Vcard::Maker.make2 do |vc|
-            vc.add_name do |name|
-              name.given = contributor.album.name
-            end
-            vc.add_email contributor.album.short_email
-          end
-
-          #attachments['album.vcf'] = vcard.to_s
-          #attachments['album.vcf'] = {:mime_type => 'text/x-vcard',:content =>vcard.to_s}
-
-          #mail( :to        => contributor.email,
-          #      :reply_to  => contributor.album.long_email,
-          #      :subject   => "You have been invited to contribute photos to '#{contributor.album.name}'!" )
+          merge_vars ={ :FNAME     => user.first_name,
+                        :LNAME     => user.last_name,
+                        :A_NAME    => album.name,
+                        :A_URL     => album_url( album ),
+                        :A_PICON   => album.picon_url,
+                        :A_EMAIL   => album.short_email }
+          ZZ::MailChimp::Message.new('contributors_added', contributor.email, merge_vars)
         end
 
 
         def upload_batch_finished( batch_id )
           batch = UploadBatch.find( batch_id )
-
-          vcard = Vpim::Vcard::Maker.make2 do |vc|
-            vc.add_name do |name|
-              name.given = batch.album.name
-            end
-            vc.add_email batch.album.short_email
-          end
-
           merge_vars ={ :F_NAME    => batch.user.first_name,
                         :L_NAME    => batch.user.last_name,
                         :A_NAME    => batch.album.name,
-                        :A_URL     => album_url( batch.album.name ),
+                        :A_URL     => album_url( batch.album),
                         :A_PICON   => batch.album.picon_url,
                         :A_EMAIL   => batch.album.short_email }
           ZZ::MailChimp::Message.new('upload_batch_finished', batch.user.email, merge_vars)
         end
 
         def album_shared_with_you(from_user_id,to_address,album_id, message)
-          @from_user = User.find(from_user_id)
-          @album = Album.find(album_id)
-          @message = message
-
-          merge_vars ={ :FROM_NAME => @from_user.name,
+          from_user = User.find(from_user_id)
+          album = Album.find(album_id)
+          merge_vars ={ :FROM_NAME => from_user.name,
                         :MESSAGE   => message,
-                        :A_PICON   => @album.picon_url,
-                        :A_URL     => album_url( @album),
-                        :A_NAME    => @album.name }
+                        :A_PICON   => album.picon_url,
+                        :A_URL     => album_url( album),
+                        :A_NAME    => album.name }
           ZZ::MailChimp::Message.new('album_shared_with_you', to_address, merge_vars)
         end
 
         def you_are_being_followed( follower_id, followed_id)
-          @follower = User.find( follower_id )
-          @followed = User.find( followed_id )
-          #mail( :to      => @followed.email,
-          #      :subject => "#{@follower.name} thinks the world of you")
+          follower = User.find( follower_id )
+          followed = User.find( followed_id )
+          raise Error 'Mail Message Not implemented yet. See ZZ::MailChimp::Notifier'
         end
 
         def activation_instructions(user_id)
           user = User.find(user_id)
-          @account_activation_url = activate_url(user.perishable_token)
-          #mail( :to      => user.email,
-          #      :subject => "Account Activation Instructions for your ZangZing Account" )
+          account_activation_url = activate_url(user.perishable_token)
+          raise Error 'Mail Message Not implemented yet. See ZZ::MailChimp::Notifier'
         end
 
         def password_reset_instructions(user_id)
           user = User.find(user_id)
-          @edit_password_reset_url = edit_password_reset_url(user.perishable_token)
-          merge_vars ={ :RESET_URL => @edit_password_reset_url }
+          merge_vars ={ :RESET_URL => edit_password_reset_url(user.perishable_token) }
           ZZ::MailChimp::Message.new('password_reset_instructions', user.email, merge_vars)
         end
 
         def welcome(user_id)
+          # Signs the user up for the service_user email list with the send-welcome-email flag set
+          # MailChimp sends the lists welcome email and adds the user to the list
           user = User.find(user_id)
-          @root_url = root_url
-          #mail( :to       => user.email,
-          #      :subject  => "Welcome to ZangZing!" )
+          merge_vars ={ :FNAME    => user.first_name,
+                        :LNAME    => user.last_name,
+                        :UNAME    => user.username,
+                        :U_URL    => user_url( user ),
+                        :U_SIGNUP => Time.now().strftime('%Y-%m-%d') }
+          ZZ::MailChimp::WelcomeMessage.new('welcome', user.email, merge_vars)
         end
 
         def test_email( to )
