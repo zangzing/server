c83abe61a8ad07a74244bd9f683baf03b28eb984
jeremyhermann <jeremyhermann@gmail.com>
2011-03-04 09:24:32 -0800

    another fix to profile photo picker

--- public/javascripts/zz/pages.js
+++ public/javascripts/zz/pages.js
@@ -653,7 +653,7 @@ pages.account_settings_profile_tab = {
         //refresh album cover picker
         $.ajax({
             dataType: 'json',
-            url: '/albums/' + zz.album_id + '/photos_json?' + zz.album_lastmod,
+            url: '/albums/' + zz.album_id + '/photos_json?' + (new Date()).getTime(),  //force browser cache miss
             success: function(json){
                 var selectedIndex=-1;
                 var currentId = $('#profile-photo-id').val();
