f58e665c4c8c9b3b7bcfaa0f4626195e1bcad7dc
duhast <shtepsel_duhast@mail.ru>
2011-03-05 21:36:05 +0200

    #708 fixed; proper service_timeouts.yml

--- app/controllers/connector/facebook_controller.rb
+++ app/controllers/connector/facebook_controller.rb
@@ -25,9 +25,18 @@ protected
 
 
   def get_photo_url(photo_info, size)
-    urls = photo_info[:images].map{|i| i[:source]}
+    images = photo_info[:images]
     sz_letter = PHOTO_SIZES[size].is_a?(Array) ? "(#{PHOTO_SIZES[size].join('|')})" : PHOTO_SIZES[size]
-    urls.select{|url| url =~ /_#{sz_letter}\.\w+$/ }.first
+    result = images.select{|img| img[:source] =~ /_#{sz_letter}\.\w+$/ }.first
+    unless result #In case of new url scheme or other failure
+      images.sort_by { |img| img[:width].to_i * img[:height].to_i } #Larger are first
+      result = case size
+      when :thumb then images[-2] #Last image has too poor resolution, so we'll pick second from the end
+      when :screen then images[-4] || images.first
+      when :full then images.first #Either original or largest of available
+      end
+    end
+    result[:source]
   end
   
   def make_source_guid(photo_info)
