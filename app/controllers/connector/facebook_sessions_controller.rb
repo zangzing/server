5d4e96735a37c82a2177edaafa6e7b7b6966aed5
jeremyhermann <jeremyhermann@gmail.com>
2011-03-06 18:59:22 -0800

    fixed 561-when-connecting-an-account-to-facebook-include-permissions-to-access-their-friends-list

--- app/controllers/connector/facebook_sessions_controller.rb
+++ app/controllers/connector/facebook_sessions_controller.rb
@@ -3,7 +3,7 @@ class Connector::FacebookSessionsController < Connector::FacebookController
   skip_before_filter :require_user, :only => [:new, :create]
 
   def new
-    auth_url = HyperGraph.authorize_url(FACEBOOK_API_KEYS[:app_id], create_facebook_session_url(:host => Server::Application.config.application_host), :scope => 'user_photos,user_photo_video_tags,friends_photo_video_tags,friends_photos,publish_stream,offline_access', :display => 'popup')
+    auth_url = HyperGraph.authorize_url(FACEBOOK_API_KEYS[:app_id], create_facebook_session_url(:host => Server::Application.config.application_host), :scope => 'user_photos,user_photo_video_tags,friends_photo_video_tags,friends_photos,publish_stream,offline_access,read_friendlists', :display => 'popup')
     redirect_to auth_url
   end
 
@@ -23,3 +23,43 @@ class Connector::FacebookSessionsController < Connector::FacebookController
   end
 
 end
+
+
+# other permissions we might consider
+#user_about_me
+#user_activities
+#user_birthday
+#user_events
+#user_groups
+#user_interests
+#user_likes
+#user_location
+#user_photo_video_tags
+#user_photos
+#user_relationships
+#user_videos
+#user_website
+#user_work_history
+#email
+#read_friendlists
+#read_stream
+#user_checkins
+#user_address
+#user_mobile_phone
+#friends_about_me
+#friends_activities
+#friends_birthday
+#friends_events
+#friends_groups
+#friends_interests
+#friends_likes
+#friends_location
+#friends_photo_video_tags
+#friends_photos
+#friends_relationships
+#friends_videos
+#friends_website
+#friends_work_history
+#manage_friendlists
+#friends_checkins
+
