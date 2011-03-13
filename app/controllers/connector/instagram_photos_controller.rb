880d82cac075cc68632080fef130136db2203774
duhast <shtepsel_duhast@mail.ru>
2011-03-13 15:31:48 +0200

    #886 fixed

--- app/controllers/connector/instagram_photos_controller.rb
+++ app/controllers/connector/instagram_photos_controller.rb
@@ -5,7 +5,7 @@ class Connector::InstagramPhotosController < Connector::InstagramController
     photo_list.reject! { |item| item[:type] != 'image' }
     photos = photo_list.map { |p|
       {
-        :name => p[:caption] || '',
+        :name => (p[:caption][:text] rescue ''),
         :id   => p[:id],
         :type => 'photo',
         :thumb_url => p[:images][:thumbnail][:url],
@@ -23,7 +23,7 @@ class Connector::InstagramPhotosController < Connector::InstagramController
     photo_data = client.media_item(params[:photo_id])
     current_batch = UploadBatch.get_current(current_user.id, params[:album_id])
     photo = Photo.create(
-            :caption => photo_data[:caption] || '',
+            :caption => (photo_data[:caption][:text] rescue ''),
             :album_id => params[:album_id],
             :user_id => current_user.id,
             :upload_batch_id => current_batch.id,
