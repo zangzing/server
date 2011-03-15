348aacebee30acfb0e475f2855b29b49465d097d
jeremyhermann <jeremyhermann@gmail.com>
2011-03-15 15:14:38 -0700

    fix problem with view buttons when running on http://photos...

--- public/javascripts/zz/zz.inits.js
+++ public/javascripts/zz/zz.inits.js
@@ -47,7 +47,7 @@ zz.init = {
             ZZAt.track('button.home.click');
         });
 
-        if (document.location.href.indexOf("/photos") !== -1) {
+        if (document.location.href.indexOf("/photos") !== -1 && document.location.href.indexOf("/photos") !== 6 /* in case we are deployed on http://photos...*/) {
             $('#header #view-buttons #grid-view-button').addClass('selected');
         }
         else if (document.location.href.indexOf("/people") !== -1) {
