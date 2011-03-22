87777b6018dad125e47f22b81532a18f03ba6d7c
gregseitz <gseitz@gmail.com>
2011-03-21 21:14:06 -0700

    add indices

--- db/migrate/20110322033910_add_upload_batch_indices.rb
+++ db/migrate/20110322033910_add_upload_batch_indices.rb
@@ -1,7 +1,7 @@
 class AddUploadBatchIndices < ActiveRecord::Migration
   def self.up
     add_index "upload_batches", ["created_at"]
-    add_index "upload_batches", ["updated_at"]
+    add_index "upload_batches", ["updated_at", "state"]
     add_index "upload_batches", ["state"]
   end
 
