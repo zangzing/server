b3c69732318b6c35411339e34e2deb517510c11d
gregseitz <gseitz@gmail.com>
2011-03-24 10:35:41 -0700

    Add instrumentation for external commands.

--- lib/zz/command_line_runner.rb
+++ lib/zz/command_line_runner.rb
@@ -1,25 +1,36 @@
 module ZZ
   # simple class that encapsulates running commands on the command line
   class CommandLineRunner
+    include NewRelic::Agent::Instrumentation::ControllerInstrumentation
+
+    # used so we can have a single instance and wrap with
+    # NewRelic instrumentation
+    def self.get_instance
+      @@runner ||= CommandLineRunner.new
+    end
+
     # run directly using the command without prepending
     # the command directory - relies on the path being set
     # up properly
-    def self.run_direct(the_cmd, args)
-      begin
-        cmd = the_cmd + " " + args
-        Rails.logger.info("CommandLine:" + cmd)
-        output = `#{cmd}`
-      rescue Errno::ENOENT
-        raise_not_found(cmd)
+    def run_direct(the_cmd, cmd_path, args)
+      perform_action_with_newrelic_trace( :name => 'external_command_' + the_cmd,
+                                          :params => { :args => args }) do
+        begin
+          full_cmd = cmd_path + " " + args
+          Rails.logger.info("CommandLine:" + full_cmd)
+          output = `#{full_cmd}`
+        rescue Errno::ENOENT
+          raise_not_found(cmd_path)
+        end
+        if $?.exitstatus == 127
+          raise_not_found(cmd_path)
+        end
+        if $?.exitstatus != 0
+          msg = "Command line call for #{the_cmd} failed with #{$?.exitstatus}"
+          raise ZZ::CommandLineException.new(msg)
+        end
+        return output
       end
-      if $?.exitstatus == 127
-        raise_not_found(cmd)
-      end
-      if $?.exitstatus != 0
-        msg = "Command line call for #{the_cmd} failed with #{$?.exitstatus}"
-        raise ZZ::CommandLineException.new(msg)
-      end
-      return output
     end
 
     def self.raise_not_found(cmd)
@@ -28,8 +39,8 @@ module ZZ
 
     # run a command from the command directory
     def self.run(the_cmd, args)
-      cmd = command_path + "/" + the_cmd
-      run_direct(cmd, args)
+      cmd_path = command_path + "/" + the_cmd
+      get_instance.run_direct(the_cmd, cmd_path, args)
     end
 
     def self.command_path
