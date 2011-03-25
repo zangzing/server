2d17d5cb9f15b7cf4232008bb32645a644d53fe3
jeremyhermann <jeremyhermann@gmail.com>
2011-03-25 10:18:55 -0700

    fix cross site forgery and mslive sessions controller

--- app/controllers/connector/mslive_sessions_controller.rb
+++ app/controllers/connector/mslive_sessions_controller.rb
@@ -2,6 +2,8 @@ class Connector::MsliveSessionsController < Connector::MsliveController
   skip_before_filter :service_login_required, :only => [:new, :delauth]
   skip_before_filter :require_user, :only => [:new, :delauth]
 
+  skip_before_filter :verify_authenticity_token, :only => [:delauth]
+
 
   def new
     live_api.returnurl = create_mslive_session_url
