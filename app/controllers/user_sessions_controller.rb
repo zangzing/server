b79da310e23ac0c176c231cc61ae673e6d2adfb3
jeremyhermann <jeremyhermann@gmail.com>
2011-03-27 19:02:42 -0700

    /signin redirects to homepage if already signed in

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -3,7 +3,11 @@ class UserSessionsController < ApplicationController
   before_filter :require_user, :only => :destroy
 
   def new
-    @user_session = UserSession.new
+    if ! current_user
+      @user_session = UserSession.new
+    else
+      redirect_to user_pretty_url(current_user)
+    end
   end
 
   def join
