54e29a1cbeda018c278f3125d4032e58c19c1d15
gregseitz <gseitz@gmail.com>
2011-03-31 21:00:13 -0700

    cache work

--- config/initializers/zangzing_config.rb
+++ config/initializers/zangzing_config.rb
@@ -1,9 +1,6 @@
 # need this because the recursively symbolize may not be available when we are called
 require "config/initializers/hash_extensions"
 
-# need this because the recursively symbolize may not be available when we are called
-require "config/initializers/hash_extensions"
-
 
 # this class hold onto the zangzing_environment data from the same named yml
 # use it for generic stuff that you'd like to control on a per environment basis
@@ -36,7 +33,7 @@ end
 # init files - we use RAILS_ENV since calling from rspec does not set up the rails environment
 class MemcachedConfig
   def self.config
-    # this is not configred like a typical yml file.  We currently use the engineyard format which the only relevent info is in defaults
+    # this is not configred like a typical yml file.  We currently use the engineyard format which the only relevant info is in defaults
     # later we should deploy our own version with proper style
     @@config ||= YAML::load(ERB.new(File.read(File.dirname(__FILE__) + "/../memcached.yml")).result)["defaults"].recursively_symbolize_keys!
   end
@@ -44,6 +41,21 @@ class MemcachedConfig
   # array of servers
   def self.server_list
     return config[:servers]
+  end
+end
+
+# this clsss wraps resque scheduler config - putting it here to avoid having too many
+# init files - we use RAILS_ENV since calling from rspec does not set up the rails environment
+class DatabaseConfig
+  def self.config
+    #
+    # NOTE NOTE NOTE
+    #
+    # we user RAILS_ENV here because we need this from a rspec test and don't want to pull everything in...
+    #
+    # DO NOT CHANGE
+    #
+    @@config ||= YAML::load(ERB.new(File.read(File.dirname(__FILE__) + "/../database.yml")).result)[ENV['RAILS_ENV']].recursively_symbolize_keys!
   end
 end
 
