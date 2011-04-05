ec81cd84e604204f90f3d87bc7a93d523b3f5820
jeremyhermann <jeremyhermann@gmail.com>
2011-04-04 18:17:33 -0700

    adding js asset helpers

--- public/javascripts/zz/assets.js
+++ public/javascripts/zz/assets.js
@@ -2,7 +2,7 @@ var assets = {
     url_for: function(path){
         if(zz.rails_asset_host){
             var host_num = path.length % 4;
-            return zz.rails_asset_host.replace('%d', host_num) + path + '?' + zz.rails_asset_id;
+            return 'http://' + zz.rails_asset_host.replace('%d', host_num) + path + '?' + zz.rails_asset_id;
         }
         else{
             return path + '?' + zz.rails_asset_id;
