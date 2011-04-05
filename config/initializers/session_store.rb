d3461d99c8d4c73d816409ea0fca71d4102134d9
jeremyhermann <jeremyhermann@gmail.com>
2011-04-04 20:31:06 -0700

    revertin changes to session cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -6,4 +6,4 @@
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-#Server::Application.config.session_store :active_record_store, :key => '_zangzing_session'
+Server::Application.config.session_store :active_record_store, :key => '_zangzing_session'
