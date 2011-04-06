614133df1de160f11d07baad55edff2c5d82223b
jeremyhermann <jeremyhermann@gmail.com>
2011-04-05 18:42:27 -0700

    cleanup welcome wiz

--- public/javascripts/zz/zz.inits.js
+++ public/javascripts/zz/zz.inits.js
@@ -341,7 +341,7 @@ zz.init = {
     },
 
     show_welcome_dialog: function(){
-        $('<iframe height="450" width="780" src="/static/welcome_dialog/index.html"></iframe>').zz_dialog({
+        $('<iframe frameborder="0" height="450" width="780" border="0" src="/static/welcome_dialog/index.html"></iframe>').zz_dialog({
             height: 420,
             width: 750,
             modal: true,
