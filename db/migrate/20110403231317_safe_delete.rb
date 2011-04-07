ab91f7ea160e0a77c6e591d7005066d98669dc66
gregseitz <gseitz@gmail.com>
2011-04-06 20:26:06 -0700

    add index

--- db/migrate/20110403231317_safe_delete.rb
+++ db/migrate/20110403231317_safe_delete.rb
@@ -2,6 +2,9 @@ class SafeDelete < ActiveRecord::Migration
   def self.up
     add_column :photos, :deleted_at, :datetime
     add_column :albums, :deleted_at, :datetime
+
+    add_index :photos, :deleted_at
+    add_index :albums, :deleted_at
   end
 
   def self.down
