acd3b530fedfe72f340aa7df0963ea9229870165
jeremyhermann <jeremyhermann@gmail.com>
2011-04-08 17:48:06 -0700

    fix route that broke movie view

--- config/routes.rb
+++ config/routes.rb
@@ -66,8 +66,8 @@ Server::Application.routes.draw do
     get    '/albums/:id/name_album'          => 'albums#name_album',          :as => :name_album
     get    '/albums/:id/preview_album_email' => "albums#preview_album_email", :as => :preview_album_email
     get    '/albums/:id/privacy'             => 'albums#privacy',             :as => :privacy
-    #get    '/albums/:id'                     => 'albums#show',                :as => :album
-    get    '/albums/:id/edit'                => 'albums#edit',                :as => :edit_album
+    get    '/albums/:id'                     => 'albums#show',                :as => :album
+#    get    '/albums/:id/edit'                => 'albums#edit',                :as => :edit_album
     get    '/albums/:id/close_batch'         => 'albums#close_batch',         :as => :close_batch
     put    '/albums/:id'                     => 'albums#update',              :as => :update_album
     delete '/albums/:id'                     => 'albums#destroy',             :as => :delete_album
