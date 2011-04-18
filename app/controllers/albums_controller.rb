1e8b0562df07ae54dc8f19049f8c7cefc1a0e7c6
Mauricio Alvarez <mauricio@nextstagellc.com>
2011-04-18 14:06:06 -0700

    Removed unused X-Error headers and corrected flashes to work with render and redirect as appropriate

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -301,7 +301,6 @@ class AlbumsController < ApplicationController
         @album = Album.find( params[:id ])  #will trhow exception if params[:id] is not defined or album not found
       rescue ActiveRecord::RecordNotFound => e
         flash.now[:error] = "This operation requires an album, we could not find one because: "+e.message
-        response.headers['X-Error'] = flash[:error]
         if request.xhr?
           render :status => 404
         else
