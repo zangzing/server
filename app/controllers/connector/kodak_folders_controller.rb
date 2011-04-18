2e9660039334da88333b79245c3288fb3d238e15
jeremyhermann <jeremyhermann@gmail.com>
2011-04-18 12:21:26 -0700

    handle case where kodak doesnt return any albums -- related to 1666-issue-with-kodak-connector

--- app/controllers/connector/kodak_folders_controller.rb
+++ app/controllers/connector/kodak_folders_controller.rb
@@ -5,16 +5,22 @@ class Connector::KodakFoldersController < Connector::KodakController
     SystemTimer.timeout_after(http_timeout) do
       album_list = connector.send_request('/albumList')
     end
-    albums = [album_list['Album']].flatten.select { |a| a['type']=='0' } #Looks like real albums have type attribute = 0, but who knows...
-    @folders = albums.map { |f|
-      {
-        :name => f['name'],
-        :type => "folder",
-        :id  =>  f['id'],
-        :open_url => kodak_photos_path(f['id']),
-        :add_url => kodak_folder_action_path({:kodak_album_id =>f['id'], :action => 'import'})
+
+    if album_list['Album']
+      albums = [album_list['Album']].flatten.select { |a| a['type']=='0' } #Looks like real albums have type attribute = 0, but who knows...
+      @folders = albums.map { |f|
+        {
+          :name => f['name'],
+          :type => "folder",
+          :id  =>  f['id'],
+          :open_url => kodak_photos_path(f['id']),
+          :add_url => kodak_folder_action_path({:kodak_album_id =>f['id'], :action => 'import'})
+        }
       }
-    }
+    else
+      @folders = []
+    end
+
     expires_in 10.minutes, :public => false
     render :json => JSON.fast_generate(@folders)
   end
