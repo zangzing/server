5eeeb7116434ed50e3c429a43383b2b97fca9156
jeremyhermann <jeremyhermann@gmail.com>
2011-04-28 07:53:26 -0700

    #1764 flickr-session-controller-validates-session-before-unlink

--- app/controllers/connector/flickr_sessions_controller.rb
+++ app/controllers/connector/flickr_sessions_controller.rb
@@ -1,5 +1,5 @@
 class Connector::FlickrSessionsController < Connector::FlickrController
-  skip_before_filter :service_login_required, :only => [:new, :create]
+  skip_before_filter :service_login_required, :only => [:new, :create, :destroy]
   skip_before_filter :require_user, :only => [:new, :create]
 
   def new
