e961f533d2c671363618713dee0097a7241130cd
jeremyhermann <jeremyhermann@gmail.com>
2011-04-29 07:49:02 -0700

    #1775 missing-method-errors-in-new-relic

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -5,7 +5,7 @@
 class AlbumsController < ApplicationController
   before_filter :require_user,              :except => [ :index, :show, :back_to_index, :my_albums_json, :liked_albums_json, :my_albums_public_json, :liked_albums_public_json, :liked_users_public_albums_json ]
   before_filter :require_user_json,         :only =>   [ :my_albums_json, :liked_albums_json ]
-  before_filter :require_album,             :except => [ :index, :create, :new, :show, :my_albums_json, :liked_albums_json, :my_albums_public_json, :liked_albums_public_json, :liked_users_public_albums_json  ]
+  before_filter :require_album,             :except => [ :index, :create, :new, :my_albums_json, :liked_albums_json, :my_albums_public_json, :liked_albums_public_json, :liked_users_public_albums_json  ]
   before_filter :require_album_admin_role,  :only =>   [ :destroy, :edit, :update ]
 
   # displays the "Select album type screen" used in the wizard
@@ -259,7 +259,7 @@ class AlbumsController < ApplicationController
 
   # displays all of an albums photos
   def show
-    redirect_to album_photos_url(params[:id])
+    redirect_to album_pretty_url(@album)
   end
 
   #deletes an album
