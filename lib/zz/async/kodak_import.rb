d6b499335604f7d5d10a10b07fe5a32cab560e01
jeremyhermann <jeremyhermann@gmail.com>
2011-05-05 06:39:02 -0700

    add systemtimer to kodak import

--- lib/zz/async/kodak_import.rb
+++ lib/zz/async/kodak_import.rb
@@ -17,15 +17,17 @@ module ZZ
       end
 
       def self.perform( photo_id, source_url, auth_token)
-        photo = Photo.find(photo_id)
-        if photo.assigned?
-          kodak_connector = KodakConnector.new(auth_token)
-          file = kodak_connector.response_as_file(source_url)
-          file_path = file.path
-          file.close()
-          file.validate_size
-          photo.file_to_upload = file_path
-          photo.save
+        SystemTimer.timeout_after(ZangZingConfig.config[:async_job_timeout]) do
+          photo = Photo.find(photo_id)
+          if photo.assigned?
+            kodak_connector = KodakConnector.new(auth_token)
+            file = kodak_connector.response_as_file(source_url)
+            file_path = file.path
+            file.close()
+            file.validate_size
+            photo.file_to_upload = file_path
+            photo.save
+          end
         end
       end
 
