af0d8b20ffd7b4034a91010739721f2677a79042
gregseitz <gseitz@gmail.com>
2011-05-22 08:46:10 -0700

    Update to work with latest agent changes

--- OpenRubyMine.sh
+++ OpenRubyMine.sh
@@ -2,7 +2,7 @@ export ZANG_ZING_HOME="$PWD"
 
 
 export MAGICK_HOME="$ZANG_ZING_HOME/../agent/lib/osx/imagemagick/ImageMagick-6.6.1"
-export DYLD_LIBRARY_PATH="$MAGICK_HOME/lib:$DYLD_LIBRARY_PATH"
+#export DYLD_LIBRARY_PATH="$MAGICK_HOME/lib:$DYLD_LIBRARY_PATH"
 export PATH="$MAGICK_HOME/bin:$PATH"
 if [ -z "${IMAGEMAGICK_PATH}" ]; then export IMAGEMAGICK_PATH="$MAGICK_HOME"; fi
 
