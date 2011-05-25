83459a400324834e8b4ee984e460d4e98393b78c
jeremyhermann <jeremyhermann@gmail.com>
2011-05-25 11:11:10 -0700

    remove .bmp from uploader

--- public/javascripts/zz/simple-uploader.js
+++ public/javascripts/zz/simple-uploader.js
@@ -102,7 +102,7 @@ var simple_uploader = {
 
                 // File Upload Settings
                 file_size_limit : "102400",	// 100MB
-                file_types : "*.jpg;*.jpeg;*.png;*.gif;*.tiff;*.bmp",
+                file_types : "*.jpg;*.jpeg;*.png;*.gif;*.tiff",
                 file_types_description : "Image Files",
                 file_upload_limit : "100",
                 file_queue_limit : "0",
