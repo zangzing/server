a427e878ffbbc50f35faa83d76cb90c4f9dfef99
Mauricio Alvarez <mauricio@nextstagellc.com>
2011-07-05 19:09:20 -0700

    Migration for mailing lists

---
+++ db/migrate/20110706015610_add_mailing_list.rb
@@ -0,0 +1,19 @@
+class AddMailingList < ActiveRecord::Migration
+  def self.up
+    create_table :mailing_lists, :force => true do |t|
+         t.string                 :name
+         t.string                 :mailchimp_list_id,  :null => false
+         t.string                 :category,      :null => false
+         t.timestamps
+       end
+       add_index :mailing_lists, :mailchimp_list_id, :unique => true
+       add_index :mailing_lists, :category
+
+       # Seed Data
+       MailingList.create( :name => 'Testing List', :mailchimp_list_id => '10ca465e13', :category => 'news' )
+  end
+
+  def self.down
+    drop_table
+  end
+end
