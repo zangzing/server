4a2ef491225cf96170909ad3cd14f98dd42b6d97
gregseitz <gseitz@gmail.com>
2011-07-13 12:38:18 -0700

    --

--- deploy/zz_before_migrate.rb
+++ deploy/zz_before_migrate.rb
@@ -4,17 +4,18 @@
 # that require files to exist such as template.  For that you
 # can add directly to the zz-chef-repo if needed.
 #
-# We only have one hook now to keep things simple.  This
-# hook runs right before any migrations have taken place and
-# before we have made the release directory current.  You have
-# access to the full set of configuration information via the
+# This hook is run right before the migrate.  At this point
+# we have not yet symlinked the release directory to current so
+# if something fails there should be no impact on the running
+# server.
+#
+# You have access to the full set of configuration information via the
 # zz variable.  See the file /data/app_name/shared/config/zz_app_dna.json
 # to see what information is available.  As a shortcut you also
 # have direct access to the following variables.  Generally you
 # should only use zz_release_dir and zz_shared_dir when referencing
 # the location of your deployed code.
 #
-#puts zz[:app_name]
 #puts zz_base_dir
 #puts zz_shared_dir
 #puts zz_current_dir
@@ -22,6 +23,11 @@
 #puts zz_app  (symbol)
 #puts zz_role (symbol)
 #puts zz_rails_env (symbol)
+#
+# You can access all of the configuration information via
+# the zz method.  So for instance to get the instance id you
+# would do:
+# zz[:instance_id]
 #
 #
 puts "-----ZZ PHOTOS_TEST_PREPARE_CONFIG------"
@@ -64,6 +70,10 @@ if [:solo, :app_master, :app].include?(zz_role)
   move_assets(assets)
 end
 
+# stop any resque workers if downtime
+if zz[:deploy_downtime]
+  run "sudo monit stop all -g resque_photos"
+end
 
 #Use Jammit gem to package css and javascript
 run "bundle exec jammit"
