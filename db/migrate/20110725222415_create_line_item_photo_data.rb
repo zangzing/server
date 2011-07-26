61ee2f32170d4944c62c2210ecea531f0aef0305
Mauricio Alvarez <mauricio@nextstagellc.com>
2011-07-26 16:12:58 -0700

    Removed admin assets and updated migrations for spree

--- db/migrate/20110725222415_create_line_item_photo_data.rb
+++ db/migrate/20110725222415_create_line_item_photo_data.rb
@@ -1,15 +1,18 @@
 class CreateLineItemPhotoData < ActiveRecord::Migration
   def self.up
      create_table :line_item_photo_data, :force => true do |t|
-      t.integer                :line_item_id
-      t.column                 :photo_id, :bigint
+      t.integer :line_item_id
+      t.column :photo_id, :bigint
       t.string :source_url
       t.string :crop_instructions
       t.timestamps
-    end
+     end
+
+      add_column :line_items, :photo_data_id, :bigint
   end
 
   def self.down
     drop_table :line_item_photo_data
+    remove_column :line_items, :photo_data_id
   end
 end
