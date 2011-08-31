0079f9d3b8e63da14197202aecc65519042e1cf8
jeremyhermann <jeremyhermann@gmail.com>
2011-08-31 10:37:31 -0700

    add comment

--- app/controllers/async_responses_controller.rb
+++ app/controllers/async_responses_controller.rb
@@ -6,7 +6,7 @@ class AsyncResponsesController < ApplicationController
     if body.nil?
       response.headers["x-poll-for-response"] = async_response_url(params[:response_id])
       render :json => {:message => "poll-for-response"}
-    elsif !body.is_a?(Array) && /"exception"\s*:\s*true/ =~ body
+    elsif !body.is_a?(Array) && /"exception"\s*:\s*true/ =~ body  #todo: Oleg added the Array check for the dropbox connector. not sure why
       exception_info = JSON.parse(body)
       render :json => {:message => exception_info['message']}, :status => exception_info['code']
     else
