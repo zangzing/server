811802d2dbe58420540302634cb8f670cc9dce59
jeremyhermann <jeremyhermann@gmail.com>
2011-09-02 10:02:12 -0700

    add rspec skeleton for albums controller

---
+++ spec/controllers/albums_controller_spec.rb
@@ -0,0 +1,98 @@
+require 'spec_helper'
+
+describe AlbumsController do
+  include ControllerSpecHelper
+
+  context 'when i view my homepage' do
+    describe 'My Albums section' do
+      it 'should show all my public albums' do
+        pending 'test not implemented'
+      end
+
+      it 'should show all my hidden albums' do
+        pending 'test not implemented'
+      end
+
+      it 'should show all my password albums' do
+        pending 'test not implemented'
+      end
+
+    end
+
+    describe 'Albums I Like section' do
+      it 'should show all the public albums i like' do
+        pending 'test not implemented'
+      end
+
+      it 'should show all the hidden albums i like' do
+        pending 'test not implemented'
+      end
+
+      it 'should show all the password albums i like' do
+        pending 'test not implemented'
+      end
+
+      it 'should show all the public albums of people i follow' do
+        pending 'test not implemented'
+      end
+
+      it 'should not show the hidden albums of people i follow' do
+        pending 'test not implemented'
+      end
+
+      it 'should not show the password albums of people i follow' do
+        pending 'test not implemented'
+      end
+    end
+
+  end
+
+  context 'when i view someone else\'s homepage' do
+    describe 'His Albums section' do
+      it 'should show all his public albums' do
+        pending 'test not implemented'
+      end
+
+      it 'should not show any of his hidden albums' do
+        pending 'test not implemented'
+      end
+
+      it 'should not show any of his password albums' do
+        pending 'test not implemented'
+      end
+
+    end
+
+    describe 'Album He Likes section' do
+      it 'should show all the public albums that he likes' do
+        pending 'test not implemented'
+      end
+
+      it 'should not show any of the hidden albums that he likes' do
+        pending 'test not implemented'
+      end
+
+      it 'should not show any of the password albums that he likes' do
+        pending 'test not implemented'
+      end
+
+      it 'should show all the public albums of people he follows' do
+        pending 'test not implemented'
+      end
+
+      it 'should not show any of the hidden albums of people he follows' do
+        pending 'test not implemented'
+      end
+
+      it 'should not show any of the password albums of people he follows' do
+        pending 'test not implemented'
+      end
+
+    end
+
+  end
+  
+
+
+end
+
