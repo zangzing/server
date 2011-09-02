6f4278b4c7fce3fb12c3392c7da474fcc0b6d76a
Mauricio Alvarez <mauricio@nextstagellc.com>
2011-09-01 18:32:49 -0700

    Working on spree migration

--- db/migrate/20110725180030_create_spree_tables.rb
+++ db/migrate/20110725180030_create_spree_tables.rb
@@ -1,12 +1,9 @@
 class CreateSpreeTables < ActiveRecord::Migration
-     MIGRATIONS_PATH='spree_zangzing/db/migrate'
+     MIGRATIONS_PATH="./spree_zangzing/db/migrate"
      def self.up
        ActiveRecord::ConnectionAdapters::Mysql2Adapter::NATIVE_DATABASE_TYPES[:primary_key] = "int(11) DEFAULT NULL auto_increment PRIMARY KEY"
-       Dir["#{MIGRATIONS_PATH}/[0-9]*_*.rb"].
-       sort.map{|filename|require filename}.flatten.
-       each{|class_name| const_get(class_name).up}
+       ActiveRecord::Migrator.migrate MIGRATIONS_PATH
        ActiveRecord::ConnectionAdapters::Mysql2Adapter::NATIVE_DATABASE_TYPES[:primary_key] = "BIGINT UNSIGNED DEFAULT NULL auto_increment PRIMARY KEY"
-
        # Load default data into commerce tables
        Rake::Task['db:load_dir'].invoke '../spree_zangzing/db/store_defaults'
      end
