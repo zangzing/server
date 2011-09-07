1a7ea2a0952289085c58258ef2992c8440145c0d
jeremyhermann <jeremyhermann@gmail.com>
2011-09-07 16:45:24 -0700

    fixed 2977-if-you-enter-comment-when-not-signed-in-it-should-take-you-to-join-screen-not-sign-in-screen

--- spec/integration/guest_comment_signin_spec.rb
+++ spec/integration/guest_comment_signin_spec.rb
@@ -30,4 +30,35 @@ describe 'commenting as guest' do
     response.body.should match(/this is a comment/)
 
   end
+
+  it 'should allow guest to enter comment then redirect to signin then back to comment after signin' do
+    photo = Factory.create(:photo)
+
+
+
+    # go to the photo's single pic view
+    visit photo_pretty_url(photo)
+
+    # post comment via ajax
+    post create_photo_comment_path(photo), {:comment=>{:text => 'this is a comment'}}
+
+    # should get redirect to sigining page that we handle in javascript
+    response.status.should eql(401)
+    visit "#{join_path}?return_to=#{CGI::escape(finish_create_photo_comment_path(photo))}"
+
+
+    # signin and follow redirect back to photo single pic vuew
+    post create_user_path, 'user[email]' => "test@test.zangzing.com", 'user[username]' => "testuser", 'user[password]' => "password", 'user[name]' => "Test User"
+
+    while redirect?
+      follow_redirect!
+    end
+
+    # verify that comment has been added to database
+    get photo_comments_url(photo)
+    response.body.should match(/this is a comment/)
+
+  end
+
+
 end
