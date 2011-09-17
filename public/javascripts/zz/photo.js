c75d7d89c18d617549511e91b17d3bd92e94af69
jeremyhermann <jeremyhermann@gmail.com>
2011-09-16 21:33:35 -0700

    bring infomenu.js up to date. for some reason we had super-old version in spree branch

--- public/javascripts/zz/photo.js
+++ public/javascripts/zz/photo.js
@@ -342,7 +342,8 @@ zz.template_cache = zz.template_cache || {};
                     var buy_button = button_bar.find('.buy-button');
                     buy_button.click(function(){
                         ZZAt.track('photo.buy.frame.click');
-                        alert('This feature is still under construction.');
+                        zz.buy.select_photo(o.photoId, self.element);
+                        zz.buy.activate_buy_mode();
                     });
 
 
