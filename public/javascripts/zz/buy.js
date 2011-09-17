52f57f780f9e0f831bff1f40d103feae30edc647
jeremyhermann <jeremyhermann@gmail.com>
2011-09-16 19:01:19 -0700

    animate photos to buy

--- public/javascripts/zz/buy.js
+++ public/javascripts/zz/buy.js
@@ -38,6 +38,64 @@ var zz = zz || {};
         });
     };
 
+    zz.buy.select_photo = function(photo_id, element, callback){
+        zz.routes.call_add_to_cart( photo_id, function(){
+            $("<div id='flash-dialog'><div><div id='flash'></div><a id='checkout' class='newgreen-button'><span>Checkout</span></a><a id='ok' class='newgreen-button'><span>OK</span></a></div></div>").zz_dialog({ autoOpen: false });
+            $('#flash-dialog #flash').text('Your photo has been added to the cart');
+            $('#ok').click( function(){ $('#flash-dialog').zz_dialog('close').empty().remove(); });
+            $('#checkout').css({ position: 'absolute', bottom: '30px', left: '40px', width: '80px' })
+             .click( function(){ window.location = '/store/cart'  });
+            $('#flash-dialog').zz_dialog('open');
+        });
+
+
+        var imageElement;
+
+        if (element.hasClass('add-all-button')) {
+            imageElement = element;
+        }
+        else {
+            imageElement = element.find('.photo-image');
+        }
+
+
+        var start_top = imageElement.offset().top;
+        var start_left = imageElement.offset().left;
+
+        var end_top = $('#footer #buy-button').offset().top;
+        var end_left = $('#footer #buy-button').offset().left;
+
+
+        var on_finish_animation = function() {
+            if(callback){
+                callback();
+            }
+            $(this).remove();
+        }
+
+        imageElement.clone()
+                .css({position: 'absolute', left: start_left, top: start_top, border: '1px solid #ffffff'})
+                .appendTo('body')
+                .addClass('animate-photo-to-tray')
+                .animate({
+                             width: '20px',
+                             height: '20px',
+                             top: (end_top) + 'px',
+                             left: (end_left) + 'px'
+                         }, 1000, 'easeInOutCubic', on_finish_animation);
+
+
+
+
+
+
+
+
+
+    };
+
+
+
 
     function open_drawer(callback){
         $('#article').fadeOut('fast', function(){
