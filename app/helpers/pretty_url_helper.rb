16bd5d75397b42e600d2e8cafdd81efd102e47ba
Mauricio Alvarez <mauricio@nextstagellc.com>
2011-09-27 09:57:25 -0700

    Added old version of simple_format as zz_simple_format for backwards compatibility with rails 3.0.0 and emails

--- app/helpers/pretty_url_helper.rb
+++ app/helpers/pretty_url_helper.rb
@@ -89,4 +89,20 @@ module PrettyUrlHelper
       URI::escape(url)
   end
 
+  # I added this method because the implementation from3.0.0 to
+  # 3.0.9 changed and it now breaks the emails, when we figure out
+  # how to deal with the new implementation of simple format we can
+  # delete this.
+  def zz_simple_format(text, html_options={}, options={})
+        text = '' if text.nil?
+        start_tag = tag('p', html_options, true)
+        text = sanitize(text) unless options[:sanitize] == false
+        text = text.to_str
+        text.gsub!(/\r\n?/, "\n")                    # \r\n and \r -> \n
+        text.gsub!(/\n\n+/, "</p>\n\n#{start_tag}")  # 2+ newline  -> paragraph
+        text.gsub!(/([^\n]\n)(?=[^\n])/, '\1<br />') # 1 newline   -> br
+        text.insert 0, start_tag
+        text.html_safe.safe_concat("</p>")
+  end
+
 end
