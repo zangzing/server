8dd0cc03f85c18e324de52298c21c12521fe0970
Mauricio Alvarez <mauricio@nextstagellc.com>
2011-09-27 16:50:00 -0700

    Added protection to image_decorator to avoid Server undefined when raking db:migrate. Updated ReferenceDataMover to eventually be used in a controller

--- lib/reference_data_mover.rb
+++ lib/reference_data_mover.rb
@@ -1,29 +1,20 @@
 class ReferenceDataMover
 
+  COMMERCE_DIR= 'catalog_export'
+  EMAIL_DIR=    'email_export'
+
   def initialize
     #Initialize connection to S3
     s3_keys = YAML.load(File.read("#{Rails.root}/config/s3.yml"))[Rails.env]
     @s3 = RightAws::S3.new(s3_keys['access_key_id'], s3_keys['secret_access_key'], {:logger       =>Logger.new(STDOUT)})
     @bucket = @s3.bucket('products.zz')
     @db_config = Rails.application.config.database_configuration[Rails.env]
-    @deploy_group = Server::Application.config.deploy_environment.zz[:deploy_group_name]
+    @deploy_group = ZZDeployEnvironment.env.zz[:deploy_group_name]
     @deploy_group = (@deploy_group && @deploy_group.length > 0 ? @deploy_group : 'localhost')
   end
 
   def export_commerce( tag='' )
-    # we use mysqldump to export the catalog,
-    # manual is here http://dev.mysql.com/doc/refman/5.6/en/mysqldump.html
-
-    @output_file = Time.now().strftime( "%Y%m%d_%H%M%S_#{@deploy_group}_#{tag}.sql")
-    tmpfile = Tempfile.new( @output_file )
-    tmpfile.close()
-    cmd =[]
-    cmd << "mysqldump"   #command
-    cmd << "-u#{@db_config['username']}"
-    cmd << " -p#{@db_config['password']}" if @db_config['password']
-    cmd << ( @db_config['host'] ? " -h#{@db_config['host']}" : "-h localhost" )
-    cmd << "#{@db_config['database']}"
-    cmd << %w(
+    tables = %w(
     assets
     calculators
     countries
@@ -57,23 +48,50 @@ class ReferenceDataMover
     zone_members
     zones
     )  #tables
-    cmd << "> #{tmpfile.path}"
-    sh cmd.flatten.compact.join(" ").strip.squeeze(" ")
-    
-    tmpfile.open()
-    @bucket.put( "catalog_export/#{@output_file}",  tmpfile )
+    export( tables, COMMERCE_DIR,  tag)
   end
 
   def commerce_export_file_list
-    @bucket.keys('prefix' => 'catalog_export').map{ |k| k.to_s }
+      @bucket.keys('prefix' => COMMERCE_DIR).map{ |k| k.to_s }
   end
 
-  def import_commerce( export_filename )
+  def export_email( tag='' )
+      tables = %w(
+      emails
+      email_templates
+      )  #tables
+      export( tables, EMAIL_DIR,  tag)
+  end
+
+  def email_export_file_list
+        @bucket.keys('prefix' => EMAIL_DIR).map{ |k| k.to_s }
+  end
+
+  def export( tables, export_dir='table_export', tag='' )
+    # we use mysqldump to export the tables
+    # manual is here http://dev.mysql.com/doc/refman/5.6/en/mysqldump.html
+    @output_file = Time.now().strftime( "%Y%m%d_%H%M%S_#{@deploy_group}_#{tag}.sql")
+    tmpfile = Tempfile.new( @output_file )
+    tmpfile.close()
+    cmd =[]
+    cmd << "mysqldump"   #command
+    cmd << "-u#{@db_config['username']}"
+    cmd << " -p#{@db_config['password']}" if @db_config['password']
+    cmd << ( @db_config['host'] ? " -h#{@db_config['host']}" : "-h localhost" )
+    cmd << "#{@db_config['database']}"
+    cmd << tables
+    cmd << "> #{tmpfile.path}"
+    sh cmd.flatten.compact.join(" ").strip.squeeze(" ")
+    tmpfile.open()
+    @bucket.put( "#{export_dir}/#{@output_file}",  tmpfile )
+  end
+
+  def import( export_filename )
     key = @bucket.key( export_filename )
-    sqlfile = Tempfile.new( key.to_s.gsub('catalog_export/','') )
+    sqlfile = Tempfile.new( key.to_s.gsub( /.*_export\//,'') )
     sqlfile.write( key.get() )
     sqlfile.close()
-    
+
     cmd =[]
     cmd << "mysql"   #command
     cmd << "-u#{@db_config['username']}"
@@ -85,4 +103,5 @@ class ReferenceDataMover
     cmd << "< #{sqlfile.path}"
     sh cmd.flatten.compact.join(" ").strip.squeeze(" ")
   end
+
 end
--- spree_zangzing/lib/tasks/commerce.rake
+++ spree_zangzing/lib/tasks/commerce.rake
@@ -1,6 +1,5 @@
 require 'readline'
 
-
 namespace :commerce do
 
   desc "Dump and Export Catalog to S3"
@@ -20,7 +19,31 @@ namespace :commerce do
     print "Enter # of file  you would like to import?"
     index = Readline.readline().to_i
     key = keys[index -1]
-    rdm.import_commerce( key )
+    rdm.import( key )
+  end
+
+end
+
+namespace :email do
+
+  desc "Dump and Export Email System Tables to S3"
+  task :export => :environment do
+    rdm = ReferenceDataMover.new()
+    rdm.export_email('rake')
+  end
+
+  desc "Import Email System Tables from S3"
+  task :import => :environment do
+    rdm = ReferenceDataMover.new()
+    keys = rdm.email_export_file_list()
+
+    keys.each_with_index do |key , i|
+      puts " #{i+1}.- #{key.gsub(/.*_export\//,'')}"
+    end
+    print "Enter # of file  you would like to import?"
+    index = Readline.readline().to_i
+    key = keys[index -1]
+    rdm.import( key )
   end
 
 end
