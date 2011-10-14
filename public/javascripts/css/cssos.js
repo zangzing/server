ce2e6328d66dbc1f7fced558388d4f4b5602765e
jeremyhermann <jeremyhermann@gmail.com>
2011-10-14 09:11:26 -0700

    3352-ui-new-buttons-using-arial-look-great-but-seem-to-show-helevetica-version-first

---
+++ public/javascripts/css/cssos.js
@@ -0,0 +1,7 @@
+if (navigator.appVersion.indexOf("Mac")!=-1) {
+    document.getElementsByTagName('html')[0].className += ' os-mac';
+}
+else if (navigator.appVersion.indexOf("Win")!=-1) {
+    document.getElementsByTagName('html')[0].className += ' os-win';
+
+}
--- public/javascripts/zz/cssos.js
+++
@@ -1,7 +0,0 @@
-if ($.client.os == 'Mac') {
-    $('html').addClass('os-mac');
-}
-else if ($.client.os == 'Windows') {
-    $('html').addClass('os-win');
-
-}
