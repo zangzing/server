198f5cf45872479faf4ab4d29e660337c21d7a61
jeremyhermann <jeremyhermann@gmail.com>
2011-11-02 08:49:23 -0700

    fix product sort order

--- spree_zangzing/app/controllers/products_controller_decorator.rb
+++ spree_zangzing/app/controllers/products_controller_decorator.rb
@@ -3,8 +3,7 @@ ProductsController.class_eval do
   respond_to :json, :only =>[ :index ]
 
   def index
-    @searcher = Spree::Config.searcher_class.new(params)
-    @products = @searcher.retrieve_products
+    @products = Product.where('deleted_at' => nil).order('created_at')
 
     expires_in 10.minutes, :public => true
 
