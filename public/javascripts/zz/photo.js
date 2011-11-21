0941959d22b8a89a302846843ce3808f0001564b
jeremyhermann <jeremyhermann@gmail.com>
2011-11-21 13:26:33 -0800

    new uploading and error indicators

--- public/javascripts/zz/photo.js
+++ public/javascripts/zz/photo.js
@@ -162,13 +162,13 @@ zz.template_cache = zz.template_cache || {};
 
             //uploading glyph
             if (o.isUploading && !o.isError) {
-                self.uploadingElement = $('<div class="photo-uploading-icon">');
+                self.uploadingElement = $('<div class="photo-status-scrim"></div><div class="photo-status-bar uploading">Uploading</div>');
                 self.borderElement.append(self.uploadingElement);
             }
 
             //error glyph
             if (o.isError) {
-                self.errorElement = $('<div class="photo-error-icon">');
+                self.errorElement = $('<div class="photo-status-scrim"></div><div class="photo-status-bar error">Error</div>');
                 self.borderElement.append(self.errorElement);
             }
 
