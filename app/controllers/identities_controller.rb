7327f983bc0952e34d2f9912ddb8107873320c42
jeremyhermann <jeremyhermann@gmail.com>
2011-12-06 11:56:27 -0800

    address gregs comments about filters and comments

--- app/controllers/identities_controller.rb
+++ app/controllers/identities_controller.rb
@@ -1,8 +1,34 @@
 class IdentitiesController < ApplicationController
 
-  before_filter :require_user
+  # Returns the specified identity object for the current user
+  #
+  # This is called as:
+  #
+  # /zz_api/identities/:service_name
+  #
+  # for example: /zz_api/identities/:facebook
+  #
+  #
+  # Returns identity object in the following form
+  #
+  #   {
+  #      "name":null,
+  #      "identity_source":"facebook",
+  #      "created_at":"2011-12-05T15:29:01-08:00",
+  #      "updated_at":"2011-12-05T15:29:01-08:00",
+  #      "id":109900075691,
+  #      "user_id":1149,
+  #      "type":"FacebookIdentity",
+  #      "last_import_all":null,
+  #      "credentials":null,
+  #      "last_contact_refresh":null
+  #   }
+
+
 
   def zz_api_identity
+    return unless require_user
+
     zz_api do
       identity = current_user.identities.find(:first, :conditions => {:identity_source => params[:service_name]})
       attrs = identity.attributes
@@ -10,7 +36,39 @@ class IdentitiesController < ApplicationController
     end
   end
 
+
+
+
+
+  # Returns all the identity objects for the current user
+  # Returns all the identity objects for the current user
+  #
+  # This is called as:
+  # This is called as:
+  #
+  # /zz_api/identities
+  #
+  # returns array of identity objects for the current
+  # user in the following form
+  #
+  # [
+  #   {
+  #      "name":null,
+  #      "identity_source":"facebook",
+  #      "created_at":"2011-12-05T15:29:01-08:00",
+  #      "updated_at":"2011-12-05T15:29:01-08:00",
+  #      "id":109900075691,
+  #      "user_id":1149,
+  #      "type":"FacebookIdentity",
+  #      "last_import_all":null,
+  #      "credentials":null,
+  #      "last_contact_refresh":null
+  #   }
+  # ]
+
   def zz_api_identities
+    return unless require_user
+
     zz_api do
       results = []
 
