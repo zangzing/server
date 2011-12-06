7edb68c405e4c7fa28294940a80f1b13307ad7ad
Mauricio Alvarez <mauricio@zangzing.com>
2011-12-06 09:16:35 -0800

    Added Filters

--- public/javascripts/zz/homepage.js
+++ public/javascripts/zz/homepage.js
@@ -8,69 +8,196 @@ zz.homepage = {};
 
 
     var cell = $('<div class="album-cell"></div>');
-    var my_albums      = null;
-    var liked_albums   = null;
-    var invited_albums = null
-    var wanted_subjects = {};
+    var my_albums        = null;
+    var liked_albums     = null;
+    var following_albums = null;
+    var invited_albums   = null;
+    var all_albums       = null;
 
-    zz.homepage.render = function(current_users_home_page, my_albums_path, session_user_liked_albums_path, liked_albums_path, liked_users_albums_path, invited_albums_path, session_user_invited_albums_path, current_user_membership) {
+    var current_users_home_page,
+        my_albums_path,
+        session_user_liked_albums_path,
+        liked_albums_path,
+        liked_users_albums_path,
+        invited_albums_path,
+        session_user_invited_albums_path,
+        current_user_membership,
+        all_albums_title,
+        my_albums_title,
+        liked_albums_title,
+        invited_albums_title,
+        following_albums_title;
 
-        // My Albums
-        call_and_merge([my_albums_path, session_user_liked_albums_path, session_user_invited_albums_path], function(albums) {
-            //show only albums for the current homepage
-            albums = _.filter(albums, function(album) {
-                return album.user_id == zz.page.displayed_user_id;
-            });
 
-            add_picons( albums );
-            my_albums = albums;
-            render_section($('#my-albums'), albums);
-        });
+    zz.homepage.init = function( p_current_users_home_page,
+                                 p_my_albums_path,
+                                 p_session_user_liked_albums_path,
+                                 p_liked_albums_path,
+                                 p_liked_users_albums_path,
+                                 p_invited_albums_path,
+                                 p_session_user_invited_albums_path,
+                                 p_current_user_membership,
+                                 p_all_albums_title,
+                                 p_my_albums_title,
+                                 p_invited_albums_title,
+                                 p_liked_albums_title,
+                                 p_following_albums_title) {
 
-        // Albums I Like
-        call_and_merge([liked_albums_path, liked_users_albums_path], function(albums) {
-            add_picons( albums );
-            liked_albums = albums;
-            render_section($('#liked-albums'), albums);
-        });
-
-        // Albums I have been invited to
-        if(current_users_home_page){
-            call_and_merge([invited_albums_path], function(albums) {
-                add_picons( albums );
-                invited_albums = albums;
-                render_section($('#invited-albums'), albums);
-            });
-        }
-        zz.like.add_id_array(wanted_subjects);
+        current_users_home_page = p_current_users_home_page;
+        my_albums_path = p_my_albums_path;
+        session_user_liked_albums_path = p_session_user_liked_albums_path;
+        liked_albums_path = p_liked_albums_path;
+        liked_users_albums_path = p_liked_users_albums_path;
+        invited_albums_path = p_invited_albums_path;
+        session_user_invited_albums_path = p_session_user_invited_albums_path;
+        current_user_membership = p_current_user_membership;
+        all_albums_title = p_all_albums_title;
+        my_albums_title = p_my_albums_title;
+        invited_albums_title = p_invited_albums_title;
+        liked_albums_title = p_liked_albums_title;
+        following_albums_title = p_following_albums_title;
+        
+        filter_sort_and_render( );
         $('#article').touchScrollY();
-        arm_sort_buttons();
+        arm_buttons();
     };
 
+
     zz.homepage.sort_by_caption_asc = function(){
-        if( _.isUndefined( my_albums ) ){
-            alert('You must render the homepage first before you sort it by caption');
-        }
-        sort_and_render( alpha_caption_asc_comprarator )
+        jQuery.cookie('zz.homepage.sort', 'caption_asc', {path:'/'});
+        filter_sort_and_render( );
     };
 
     zz.homepage.sort_by_updated_at_desc = function(){
-        if( _.isUndefined( my_albums ) ){
-            alert('You must render the homepage first before you sort it by caption');
-        }
-        sort_and_render( most_recent_first_comparator )
+        jQuery.cookie('zz.homepage.sort', 'updated_at_desc', {path:'/'});
+        filter_sort_and_render( );
     };
 
-    //sorts existing arrays and refreshes display
-    function sort_and_render( comparator ){
-        render_section( $('#my-albums'), _.sortBy(my_albums, comparator ) );
-        render_section($('#liked-albums'), _.sortBy(liked_albums, comparator ) );
+    zz.homepage.sort_by_created_at_asc = function(){
+        jQuery.cookie('zz.homepage.sort', 'created_at_asc', {path:'/'});
+        filter_sort_and_render( );
+    };
 
-        if( !_.isUndefined( invited_albums )){
-            render_section($('#invited-albums'), _.sortBy(invited_albums, comparator ) );
+    zz.homepage.show_all_albums = function(){
+        jQuery.cookie('zz.homepage.filter', 'all', {path:'/'});
+        filter_sort_and_render( );
+    };
+
+    zz.homepage.show_my_albums = function(){
+        jQuery.cookie('zz.homepage.filter', 'my', {path:'/'});
+        filter_sort_and_render( );
+    };
+
+    zz.homepage.show_invited_albums = function(){
+        jQuery.cookie('zz.homepage.filter', 'invited', {path:'/'});
+        filter_sort_and_render( );
+    };
+
+    zz.homepage.show_liked_albums = function(){
+        jQuery.cookie('zz.homepage.filter', 'liked', {path:'/'});
+        filter_sort_and_render( );
+    };
+
+    zz.homepage.show_following_albums = function(){
+        jQuery.cookie('zz.homepage.filter', 'following', {path:'/'});
+        filter_sort_and_render( );
+    };
+
+    function filter_sort_and_render( ){
+        switch( $.cookie('zz.homepage.filter')){
+            case('my'):
+                if( my_albums ){
+                    render( my_albums_title, sort( my_albums ));
+                }else{
+                    call_and_merge([my_albums_path, session_user_liked_albums_path, session_user_invited_albums_path], function(albums) {
+                        //show only albums for the current homepage
+                        albums = _.filter(albums, function(album) {
+                            return album.user_id == zz.page.displayed_user_id;
+                        });
+                        add_picons( albums );
+                        my_albums = albums;
+                        render(my_albums_title, my_albums);
+                        fetch_like_info(my_albums);
+                    });
+
+                }
+                break;
+            case('invited'):
+                if(current_users_home_page){
+                    if( invited_albums ){
+                        render( invited_albums_title, sort( invited_albums ));
+                    }else{
+                        call_and_merge([invited_albums_path], function(albums) {
+                            add_picons( albums );
+                            invited_albums = albums;
+                            render(invited_albums_title, invited_albums);
+                            fetch_like_info(invited_albums);
+                        });
+                    }
+                }
+                break;
+            case('liked'):
+                if( liked_albums ){
+                    render(liked_albums_title, sort( liked_albums) );
+                }else{
+                    call_and_merge([liked_albums_path], function(albums) {
+                        add_picons( albums );
+                        liked_albums = albums;
+                        render(liked_albums_title, liked_albums);
+                        fetch_like_info(liked_albums);
+                    });
+                }
+                break;
+            case('following'):
+                if( following_albums ){
+                    render(following_albums_title, sort( following_albums) );
+                }else{
+                    call_and_merge([ liked_users_albums_path ], function(albums) {
+                        add_picons( albums );
+                        following_albums = albums;
+                        render(following_albums_title,following_albums);
+                        fetch_like_info(following_albums);
+                    });
+                }
+                break;
+            case('all'):
+            default: //all
+                if( all_albums ){
+                     render(all_albums_title, sort( all_albums ) );
+                }else{
+                    var all_urls = [
+                        my_albums_path,
+                        session_user_liked_albums_path,
+                        session_user_invited_albums_path,
+                        liked_albums_path,
+                        liked_users_albums_path
+                    ];
+
+                    if( current_users_home_page ){
+                        all_urls.push( invited_albums_path );
+                    }
+                    call_and_merge( all_urls , function(albums) {
+                        add_picons( albums );
+                        all_albums = albums;
+                        render(all_albums_title,all_albums);
+                        fetch_like_info(all_albums);
+                    });
+                }
         }
     }
 
+    function sort( albums ){
+        switch( $.cookie('zz.homepage.sort')){
+                case('caption_asc'):
+                    return _.sortBy(albums, alpha_caption_asc_comprarator );
+                case('updated_at_desc'):
+                    return _.sortBy(albums, most_recent_first_comparator );
+                case('created_at_asc'):
+                    return _.sortBy(albums, most_recent_first_comparator ); //TODO: Write correct comparator
+                default:
+                    return _.sortBy(albums, most_recent_first_comparator );
+            }
+    }
 
     // gets the albums from each url in the urls array
     // calls back with the merged,sorted,deduped array of albums
@@ -94,8 +221,8 @@ zz.homepage = {};
             });
 
             //sort
-            var sorted_results = _.sortBy(combined_results, most_recent_first_comparator );
-
+            var sorted_results = sort( combined_results );
+            
             //remove duplicates
             var previous_album = null;
             var unique_results = _.select(sorted_results, function(album) {
@@ -215,15 +342,15 @@ zz.homepage = {};
     // Cleans the container and inserts the album cells in
     // the order they are in the array.
     // Albums in the array must have picons already added ( see add_picons )
-    function render_section(container, albums) {
-
+    function render(title, albums) {
+        var container = $('div#albums');
         container.find('div.album-cell').detach();
         container.fadeOut( 'fast');
+        container.find('div#albums-title').text( title );
         var new_container = container.clone();
         _.each(albums, function(album) {
             if( !_.isUndefined( album.ui_cell ) ){
                 new_container.append( album.ui_cell );
-                wanted_subjects[album.id] = 'album';
             }
         });
         container.replaceWith( new_container );
@@ -241,15 +368,108 @@ zz.homepage = {};
                 return album.name;
     }
 
-    function arm_sort_buttons(){
+    function fetch_like_info(albums){
+         var wanted_subjects  = {};
+        _.each(albums, function(album) {
+                wanted_subjects[album.id] = 'album';
+        });
+         zz.like.add_id_array(wanted_subjects);
+    }
+
+    function arm_buttons(){
+        $('#filter-all-button').click( function(){
+            ZZAt.track('homepage.filter-all.button.click');
+            zz.homepage.show_all_albums();
+            set_button_selection();
+        });
+        $('#filter-my-button').click( function(){
+            ZZAt.track('homepage.filter-my.button.click');
+            zz.homepage.show_my_albums();
+            set_button_selection();
+        });
+        $('#filter-invited-button').click( function(){
+            ZZAt.track('homepage.filter-invited.button.click');
+            zz.homepage.show_invited_albums();
+            set_button_selection();
+        });
+        $('#filter-liked-button').click( function(){
+            ZZAt.track('homepage.filter-liked.button.click');
+            zz.homepage.show_liked_albums();
+            set_button_selection();
+        });
+        $('#filter-following-button').click( function(){
+            $(this).addClass('selected');
+            ZZAt.track('homepage.filter-following.button.click');
+            zz.homepage.show_following_albums();
+            set_button_selection();
+        });
+        $('#sort-date-button').click( function(){
+            $(this).addClass('selected');
+            ZZAt.track('homepage.sort-date.button.click');
+            zz.homepage.sort_by_created_at_asc();
+            set_button_selection();
+        });
         $('#sort-recent-button').click( function(){
+            $(this).addClass('selected');
             ZZAt.track('homepage.sort-recent.button.click');
             zz.homepage.sort_by_updated_at_desc();
+            set_button_selection();
         } );
         $('#sort-name-button').click( function(){
             ZZAt.track('homepage.sort-name.button.click');
             zz.homepage.sort_by_caption_asc();
+            set_button_selection();
         } );
+        set_button_selection();
+    }
+
+    function clear_selection(){
+        $('div#view-buttons a.gray-button').removeClass('selected');
+    }
+
+    function set_button_selection(){
+        clear_selection();
+        switch( $.cookie('zz.homepage.sort')){
+            case('caption_asc'):
+                $('#sort-name-button').addClass('selected');
+                break;
+            case('created_at_asc'):
+                $('#sort-date-button').addClass('selected');
+                break;
+            case('updated_at_desc'):
+            default:
+                $('#sort-recent-button').addClass('selected');
+        }
+        switch( $.cookie('zz.homepage.filter')){
+            case('my'):
+                $('#filter-my-button').addClass('selected');
+                break;
+            case('invited'):
+                $('#filter-invited-button').addClass('selected');
+                break;
+            case('liked'):
+                $('#filter-liked-button').addClass('selected');
+                break;
+            case('following'):
+                $('#filter-following-button').addClass('selected');
+                break;
+            case('all'):
+            default: //all
+                $('#filter-all-button').addClass('selected');
+        }
+    }
+
+    function sort( albums ){
+        switch( $.cookie('zz.homepage.sort')){
+                case('caption_asc'):
+                    return _.sortBy(albums, alpha_caption_asc_comprarator );
+                case('updated_at_desc'):
+                    return _.sortBy(albums, most_recent_first_comparator );
+                case('created_at_asc'):
+                    return _.sortBy(albums, most_recent_first_comparator ); //TODO: Write correct comparator
+                default:
+                    return _.sortBy(albums, most_recent_first_comparator );
+            }
     }
 
 })();
