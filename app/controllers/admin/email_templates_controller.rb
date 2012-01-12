0b9eb797cb639aac77bf15b30109b869a3064316
jeremyhermann <jeremyhermann@gmail.com>
2012-01-12 15:50:37 -0800

    fix email tester

--- app/controllers/admin/email_templates_controller.rb
+++ app/controllers/admin/email_templates_controller.rb
@@ -199,7 +199,8 @@ class Admin::EmailTemplatesController < Admin::AdminController
   end
 
   def test_invite_to_join(template_id)
-    Notifier.invite_to_join(sender.id, user_or_not_user_email_address, "http://www.zangzing.com/invite?ref=some_invite_token", template_id)
+    invitation = Invitation.create_invitation_for_email(sender, 'test@test.zangzing.com')
+    Notifier.invite_to_join(sender.id, user_or_not_user_email_address, invitation.tracked_link.url, template_id)
   end
 
   def test_joined_from_invite(template_id)
