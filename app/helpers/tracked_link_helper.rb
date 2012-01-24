c9e7fa7c035faab742811955ea3f6d6adb9d4d5a
jeremyhermann <jeremyhermann@gmail.com>
2012-01-24 14:44:25 -0800

    handle mangled tracjing token

--- app/helpers/tracked_link_helper.rb
+++ app/helpers/tracked_link_helper.rb
@@ -5,9 +5,11 @@ module TrackedLinkHelper
 
     if tracking_token
       tracked_link = TrackedLink.find_by_tracking_token(tracking_token)
-      TrackedLink.handle_visit(tracking_token)
-      session[:tracking_token] = tracking_token
-      redirect_to tracked_link.url
+      if tracked_link  # check in case tracking token got mangled in copy/paste
+        TrackedLink.handle_visit(tracking_token)
+        session[:tracking_token] = tracking_token
+        redirect_to tracked_link.url
+      end
     end
   end
 
