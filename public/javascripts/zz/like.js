a2c0f4f9d9ae0a3909afcfbf48805c2af5c559a1
Bowen Li <bowen@zangzing.com>
2012-02-02 13:48:51 -0800

    change wording to follow

--- public/javascripts/zz/like.js
+++ public/javascripts/zz/like.js
@@ -84,7 +84,7 @@ zz.like = {
             error: function(xhr) {
                 if (xhr.status == 401) {
                     var returnUrl = 'https://' + document.location.hostname + zz.routes.path_prefix + '/' + subject_type + 's/' + subject_id + '/like';
-                    zz.routes.users.goto_join_screen(returnUrl, "Join for free or sign in to Like.");
+                    zz.routes.users.goto_join_screen(returnUrl, "Join for free or sign in to Follow or Like.");
                 } else {
                     // toggle in server failed, return hash and screen to previous state
                     zz.like.toggle_in_hash(subject_id);
