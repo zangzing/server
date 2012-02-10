f033d0d21eeca90f02ba25ae3f58cad70d211440
gregseitz <gseitz@gmail.com>
2012-02-10 14:22:06 -0800

    reset column info before groups helper migration

--- db/migrate/20120206195722_copy_from_redis_acl.rb
+++ db/migrate/20120206195722_copy_from_redis_acl.rb
@@ -3,6 +3,8 @@ class CopyFromRedisAcl < ActiveRecord::Migration
     # bring in all the redis based acls and create appropriate
     # automatic users where acls is email based, also
     # backs up the current database
+    ActiveRecord::Base.reset_column_information
+    ActiveRecord::Base.send(:subclasses).each{|klass| klass.reset_column_information rescue nil}
     GroupsMigrationHelper.migrate_all
   end
 
