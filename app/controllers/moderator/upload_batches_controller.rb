b24fd14630527cb794d108685bcbcc9615503660
jeremyhermann <jeremyhermann@gmail.com>
2012-02-13 12:56:34 -0800

    fix sort date

--- app/controllers/moderator/upload_batches_controller.rb
+++ app/controllers/moderator/upload_batches_controller.rb
@@ -1,7 +1,7 @@
 class Moderator::UploadBatchesController < Moderator::BaseController
 
   def index
-    sql = "SELECT DISTINCT DATE_FORMAT( DATE_SUB( created_at, INTERVAL 8 HOUR), '%Y-%m-%d') as date, review_status, COUNT(*) as count FROM upload_batches WHERE state='finished' GROUP BY date, review_status ORDER BY created_at DESC"
+    sql = "SELECT DISTINCT DATE_FORMAT( DATE_SUB( created_at, INTERVAL 8 HOUR), '%Y-%m-%d') as date, review_status, COUNT(*) as count FROM upload_batches WHERE state='finished' GROUP BY date, review_status ORDER BY date DESC"
     data = UploadBatch.connection.select_all(sql)
     @upload_batches_days = {}
     data.each do |data_row|
