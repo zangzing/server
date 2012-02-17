85907f9f7f24d7989b1d69a9a61997655494c300
jeremyhermann <jeremyhermann@gmail.com>
2012-02-16 16:26:06 -0800

    fix issue with porn review

--- app/controllers/moderator/upload_batches_controller.rb
+++ app/controllers/moderator/upload_batches_controller.rb
@@ -1,7 +1,7 @@
 class Moderator::UploadBatchesController < Moderator::BaseController
 
   def index
-    sql = "SELECT DISTINCT DATE_FORMAT( DATE_SUB( created_at, INTERVAL 8 HOUR), '%Y-%m-%d') as date, review_status, COUNT(*) as count FROM upload_batches WHERE state='finished' GROUP BY date, review_status ORDER BY date DESC"
+    sql = "SELECT DISTINCT DATE_FORMAT( DATE( created_at), '%Y-%m-%d') as date, review_status, COUNT(*) as count FROM upload_batches WHERE state='finished' GROUP BY date, review_status ORDER BY date DESC"
     data = UploadBatch.connection.select_all(sql)
     @upload_batches_days = []
     current_date = nil
