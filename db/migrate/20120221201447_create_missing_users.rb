6fe8a9b43e04ed5da1629d5dede59f1ee0bcfd25
gregseitz <gseitz@gmail.com>
2012-02-21 12:28:12 -0800

    check to see if exists already

--- db/migrate/20120221201447_create_missing_users.rb
+++ db/migrate/20120221201447_create_missing_users.rb
@@ -1,17 +1,24 @@
 class CreateMissingUsers < ActiveRecord::Migration
   def self.up
     User.reset_column_information
-    user = User.new(:email => 'marketinguser@zangzing.com', :name => 'ZangZing Marketing', :username => 'zzmarketing',
-            :password => 'x739iutp', :automatic => false)
-    user.reset_perishable_token
-    user.reset_single_access_token
-    user.save!
 
-    user = User.new(:email => 'ezprintsuser@zangzing.com', :name => 'EZPrints EZPrints', :username => 'ezprintsuser',
-            :password => 'x739iutp', :automatic => false)
-    user.reset_perishable_token
-    user.reset_single_access_token
-    user.save!
+    u = User.find_by_username('zzmarketing')
+    if u.nil?
+      user = User.new(:email => 'marketinguser@zangzing.com', :name => 'ZangZing Marketing', :username => 'zzmarketing',
+              :password => 'x739iutp', :automatic => false)
+      user.reset_perishable_token
+      user.reset_single_access_token
+      user.save!
+    end
+
+    u = User.find_by_username('ezprintsuser')
+    if u.nil?
+      user = User.new(:email => 'ezprintsuser@zangzing.com', :name => 'EZPrints EZPrints', :username => 'ezprintsuser',
+              :password => 'x739iutp', :automatic => false)
+      user.reset_perishable_token
+      user.reset_single_access_token
+      user.save!
+    end
 
   end
 
