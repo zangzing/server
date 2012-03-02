cb45f129c312fd7ba94e2a8b49b31158dd198180
jeremyhermann <jeremyhermann@gmail.com>
2012-03-02 15:29:34 -0800

    turn off kiss metrics and js error capture

--- public/javascripts/zz/zzat.js
+++ public/javascripts/zz/zzat.js
@@ -56,21 +56,21 @@
         });
     }
 
-    function initKissMetrics(){
-        window._kmq = window._kmq || [];
-        function _kms(u){
-            setTimeout(function(){
-                var s = document.createElement('script');
-                var f = document.getElementsByTagName('script')[0];
-                s.type = 'text/javascript';
-                s.async = true;
-                s.src = u;
-                f.parentNode.insertBefore(s, f);
-            }, 1);
-        }
-        _kms('//i.kissmetrics.com/i.js');
-        _kms('//doug1izaerwt3.cloudfront.net/' + zz.zza_config.KISSMETRICS_TOKEN + '.1.js');
-    }
+//    function initKissMetrics(){
+//        window._kmq = window._kmq || [];
+//        function _kms(u){
+//            setTimeout(function(){
+//                var s = document.createElement('script');
+//                var f = document.getElementsByTagName('script')[0];
+//                s.type = 'text/javascript';
+//                s.async = true;
+//                s.src = u;
+//                f.parentNode.insertBefore(s, f);
+//            }, 1);
+//        }
+//        _kms('//i.kissmetrics.com/i.js');
+//        _kms('//doug1izaerwt3.cloudfront.net/' + zz.zza_config.KISSMETRICS_TOKEN + '.1.js');
+//    }
 
 
     function initZZA() {
@@ -131,7 +131,7 @@
     initZZA();
     initGoogle();
     initMixpanel();
-    initKissMetrics();
+//    initKissMetrics();
 
 })();
 
